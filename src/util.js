fbd3b8191c5e1a06c933a36e6a89e3d69b6af4a0
pubkey <daniel_van_meyer@hotmail.de>
2017-01-14 04:16:09 +0100

    ADD numberToString to util

--- src/util.js
+++ src/util.js
@@ -177,9 +177,59 @@ export function filledArray(size = 0) {
     return ret;
 }
 
+/**
+ * uppercase first char
+ * @param  {string} str
+ * @return {string} Str
+ */
 export function ucfirst(str) {
     str += '';
     const f = str.charAt(0)
         .toUpperCase();
     return f + str.substr(1);
+}
+
+
+/**
+ * @link https://de.wikipedia.org/wiki/Base58
+ * this does not start with the numbers to generate valid variable-names
+ */
+const base58Chars = 'abcdefghijkmnopqrstuvwxyzABCDEFGHJKLMNPQRSTUVWXYZ123456789';
+const base58Length = base58Chars.length;
+
+/**
+ * transform a number to a string by using only base58 chars
+ * @link https://github.com/matthewmueller/number-to-letter/blob/master/index.js
+ * @param {number} nr                                       | 10000000
+ * @return {string} the string-representation of the number | '2oMX'
+ */
+export function numberToLetter(nr) {
+    const digits = [];
+    do {
+        const v = nr % base58Length;
+        digits.push(v);
+        nr = Math.floor(nr / base58Length);
+    } while (nr-- > 0);
+
+    return digits
+        .reverse()
+        .map(d => base58Chars[d])
+        .join('');
+}
+
+/**
+ * removes trailing and ending dots from the string
+ * @param  {string} str
+ * @return {string} str without wrapping dots
+ */
+export function trimDots(str) {
+    // start
+    while (str.charAt(0) == '.')
+        str = str.substr(1);
+
+    // end
+    while (str.slice(-1) == '.')
+        str = str.slice(0, -1);
+
+    return str;
 }
