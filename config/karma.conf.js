55b9e9017605ddce56b7382365aa6b90df3ff5c9
salomonelli <sara.steiert@outlook.com>
2017-06-02 22:23:43 +0200

    ADD chrome-no-sandbox

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -34,7 +34,7 @@ module.exports = function(config) {
                 // return ['Firefox']; // comment in to test specific browser
 
                 return availableBrowser
-                    .filter(b => !['PhantomJS', 'FirefoxAurora', 'FirefoxNightly'].includes(b));
+                    .filter(b => !['PhantomJS', 'ChromeNoSandbox', 'FirefoxAurora', 'FirefoxNightly'].includes(b));
             }
         },
 
@@ -62,7 +62,12 @@ module.exports = function(config) {
             }
         },
 
-
+        customLaunchers: {
+            ChromeNoSandbox: {
+                base: 'Chrome',
+                flags: ['--no-sandbox']
+            }
+        },
         singleRun: true
     });
 };
