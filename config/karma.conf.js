34be2de7b4bb08092c2e4655bfccaac560917306
salomonelli <sara.steiert@outlook.com>
2017-06-02 22:52:19 +0200

    UPDATE captureTimeout

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -64,7 +64,7 @@ module.exports = function(config) {
 
         browserDisconnectTimeout: 10000,
         browserNoActivityTimeout: 30000,
-        captureTimeout: 100000,
+        captureTimeout: 150000,
         singleRun: true
     });
 };
