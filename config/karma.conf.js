a9cea0ca70971093417ad044561ada83636bff68
pubkey <daniel_van_meyer@hotmail.de>
2017-07-18 22:36:47 +0200

    TEST travis with chromium

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -1,76 +1,84 @@
 const tests = require('../test/unit.test');
 
+const configuration = {
+    basePath: '',
+    frameworks: [
+        'mocha',
+        'browserify',
+        'detectBrowsers'
+    ],
+    files: tests,
+    /*        files: [
+                '../test_tmp/browser/Adapters.test.js',
+                '../test_tmp/browser/RxBroadcastChannel.test.js',
+                '../test_tmp/browser/Insert.test.js',
+                '../test_tmp/browser/Observe.test.js',
+                '../test_tmp/browser/CrossInstance.test.js',
+                '../test_tmp/browser/LeaderElection.test.js'
+            ],*/
+    port: 9876,
+    colors: true,
+    autoWatch: false,
+
+    /**
+     * see
+     * @link https://github.com/litixsoft/karma-detect-browsers
+     */
+    detectBrowsers: {
+        enabled: true,
+        usePhantomJS: false,
+        postDetection: function(availableBrowser) {
+
+            // return ['Firefox']; // comment in to test specific browser
+
+            return availableBrowser
+                .filter(b => !['PhantomJS', 'FirefoxAurora', 'FirefoxNightly'].includes(b));
+        }
+    },
+
+    // Karma plugins loaded
+    plugins: [
+        'karma-mocha',
+        'karma-browserify',
+        'karma-chrome-launcher',
+        'karma-edge-launcher',
+        'karma-firefox-launcher',
+        'karma-ie-launcher',
+        'karma-opera-launcher',
+        'karma-detect-browsers'
+    ],
+
+    // Source files that you wanna generate coverage for.
+    // Do not include tests or libraries (these files will be instrumented by Istanbul)
+    preprocessors: {
+        '../test_tmp/unit/*.test.js': ['browserify']
+    },
+
+    client: {
+        mocha: {
+            bail: true,
+            timeout: 6000
+        }
+    },
+    browsers: ['ChromeNoSandbox'],
+    browserDisconnectTimeout: 6000,
+    processKillTimeout: 6000,
+    customLaunchers: {
+        Chrome_travis_ci: {
+            base: 'Chrome',
+            flags: ['--no-sandbox']
+        }
+    },
+    singleRun: true
+};
 
 module.exports = function(config) {
-    config.set({
-        basePath: '',
-        frameworks: [
-            'mocha',
-            'browserify',
-            'detectBrowsers'
-        ],
-        files: tests,
-        /*        files: [
-                    '../test_tmp/browser/Adapters.test.js',
-                    '../test_tmp/browser/RxBroadcastChannel.test.js',
-                    '../test_tmp/browser/Insert.test.js',
-                    '../test_tmp/browser/Observe.test.js',
-                    '../test_tmp/browser/CrossInstance.test.js',
-                    '../test_tmp/browser/LeaderElection.test.js'
-                ],*/
-        port: 9876,
-        colors: true,
-        autoWatch: false,
+    config.set();
+};
 
-        /**
-         * see
-         * @link https://github.com/litixsoft/karma-detect-browsers
-         */
-        detectBrowsers: {
-            enabled: true,
-            usePhantomJS: false,
-            postDetection: function(availableBrowser) {
+if (process.env.TRAVIS)
+    configuration.browsers = ['Chrome_travis_ci'];
 
-                // return ['Firefox']; // comment in to test specific browser
-
-                return availableBrowser
-                    .filter(b => !['PhantomJS', 'FirefoxAurora', 'FirefoxNightly'].includes(b));
-            }
-        },
-
-        // Karma plugins loaded
-        plugins: [
-            'karma-mocha',
-            'karma-browserify',
-            'karma-chrome-launcher',
-            'karma-edge-launcher',
-            'karma-firefox-launcher',
-            'karma-ie-launcher',
-            'karma-opera-launcher',
-            'karma-detect-browsers'
-        ],
-
-        // Source files that you wanna generate coverage for.
-        // Do not include tests or libraries (these files will be instrumented by Istanbul)
-        preprocessors: {
-            '../test_tmp/unit/*.test.js': ['browserify']
-        },
-
-        client: {
-            mocha: {
-                bail: true,
-                timeout: 6000
-            }
-        },
-        browsers: ['ChromeNoSandbox'],
-        browserDisconnectTimeout: 6000,
-        processKillTimeout: 6000,
-        customLaunchers: {
-            ChromeNoSandbox: {
-                base: 'Chrome',
-                flags: ['--no-sandbox']
-            }
-        },
-        singleRun: true
-    });
+module.exports = function(config) {
+    config.set(configuration);
 };
