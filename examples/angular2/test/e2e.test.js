ca7323295ee39c75f976c84d8448035c87ec9cff
pubkey <daniel_van_meyer@hotmail.de>
2017-09-06 18:21:05 +0200

    ADD(angular2) test for leader-election

--- examples/angular2/test/e2e.test.js
+++ examples/angular2/test/e2e.test.js
@@ -30,10 +30,10 @@ test('insert a hero', async t => {
     await t.expect(heroListElement.textContent).contains('Kelso', 'list-item contains name');
 });
 
-test.page('http://0.0.0.0:8888/multitab.html')('multitab: insert hero and check other tab', async t => {
+test.page('http://0.0.0.0:8888/multitab.html?frames=2')('multitab: insert hero and check other tab', async t => {
 
     await t
-        .switchToIframe('#frame_left')
+        .switchToIframe('#frame_0')
         .typeText('.hero-insert-component input[name=name]', 'SteveIrwin')
         .typeText('.hero-insert-component input[name=color]', 'red')
         .click('.hero-insert-component button');
@@ -41,7 +41,81 @@ test.page('http://0.0.0.0:8888/multitab.html')('multitab: insert hero and check 
     await t.switchToMainWindow();
 
     // check if in other iframe
-    await t.switchToIframe('#frame_right');
+    await t.switchToIframe('#frame_1');
     const heroListElement = Selector('.hero-list-component .mat-list-item');
     await t.expect(heroListElement.textContent).contains('Irwin', 'list-item contains name');
+});
+
+test.page('http://0.0.0.0:8888/multitab.html?frames=6')('leader-election: Exact one tab should become leader', async t => {
+
+    // wait until last tab loaded
+    await t.switchToIframe('#frame_5');
+    const heroNameInput = Selector('.hero-insert-component input[name=name]');
+    await t.typeText(heroNameInput, 'foobar');
+    await t.switchToMainWindow();
+
+    // wait until at least one becomes leader
+    let currentLeader = null;
+    await AsyncTestUtil.waitUntil(async() => {
+        let ret = false;
+        for (let i = 0; i < 6; i++) {
+            await t.switchToIframe('#frame_' + i);
+            const title = await Selector('title').innerText;
+            if (title.includes('♛')) {
+                currentLeader = i;
+                ret = true;
+            }
+            await t.switchToMainWindow();
+        }
+        return ret;
+    });
+
+    await AsyncTestUtil.wait(200); // w8 a bit
+    // ensure still only one is leader
+    let leaderAmount = 0;
+    for (let i = 0; i < 6; i++) {
+        await t.switchToIframe('#frame_' + i);
+        const title = await Selector('title').innerText;
+        if (title.includes('♛'))
+            leaderAmount++;
+        await t.switchToMainWindow();
+    }
+    if (leaderAmount !== 1)
+        throw new Error('more than one tab is leader');
+
+
+
+    /*
+        // TODO killing the leader does not work inside of iframes, this could be a bug of the unload-module
+
+        console.log('leaderAmount: ' + leaderAmount);
+        console.log('currentLeader: ' + currentLeader);
+
+        // kill the leader
+        await t
+            .typeText('#removeId', currentLeader + '')
+            .click('#submit');
+
+        // wait until next one becomes leader
+        await AsyncTestUtil.wait(200);
+        const leaders = [];
+        await AsyncTestUtil.waitUntil(async() => {
+            let ret = false;
+            for (let i = 0; i < 6; i++) {
+                if (i !== currentLeader) {
+                    await t.switchToIframe('#frame_' + i);
+                    const title = await Selector('title').innerText;
+                    console.log(title);
+                    if (title.includes('♛')) {
+                        leaders.push(i);
+                        ret = true;
+                    }
+                    await t.switchToMainWindow();
+                }
+            }
+            return ret;
+        });
+
+        console.dir(leaders);
+    */
 });
