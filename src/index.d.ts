69f75d52d67b0e547781616f2a49387a4d4039b9
Ibby Hadeed <ibby93@gmail.com>
2017-10-16 03:50:55 -0400

    Improve RxQuery typings

--- src/index.d.ts
+++ src/index.d.ts
@@ -212,24 +212,49 @@ export declare class RxCollection<RxDocumentType> {
     remove(): Promise<any>;
 }
 
+export interface RxQueryOptions<T> {
+    $eq?: T;
+    $gt?: T;
+    $gte?: T;
+    $lt?: T;
+    $lte?: T;
+    $ne?: T;
+    $in?: T[];
+    $nin?: T[];
+    $regex?: RegExp;
+    $exists?: boolean;
+    $type?: 'null' | 'boolean' | 'number' | 'string' | 'array' | 'object';
+    $mod?: number;
+    $not?: T;
+    $all?: T[];
+    $size?: number;
+    $elemMatch?: RxQueryOptions<T>;
+}
+
+export type RxQueryObject<T> = keyof T & { [P in keyof T]?: T[P] | RxQueryOptions<T[P]>; } & {
+    $or: RxQueryObject<T>[];
+    $nor: RxQueryObject<T>[];
+    $and: RxQueryObject<T>[];
+};
+
 export declare class RxQuery<RxDocumentType>{
     collection: RxCollection<RxDocumentType>;
 
-    where(queryObj: any): RxQuery<RxDocumentType>;
+    where(queryObj: RxQueryObject<RxDocumentType>): RxQuery<RxDocumentType>;
     equals(queryObj: any): RxQuery<RxDocumentType>;
     eq(queryObj: any): RxQuery<RxDocumentType>;
-    or(queryObj: any): RxQuery<RxDocumentType>;
-    nor(queryObj: any): RxQuery<RxDocumentType>;
-    and(queryObj: any): RxQuery<RxDocumentType>;
+    or(queryObj: keyof RxDocumentType): RxQuery<RxDocumentType>;
+    nor(queryObj: keyof RxDocumentType): RxQuery<RxDocumentType>;
+    and(queryObj: keyof RxDocumentType): RxQuery<RxDocumentType>;
     gt(queryObj: any): RxQuery<RxDocumentType>;
     gte(queryObj: any): RxQuery<RxDocumentType>;
     lt(queryObj: any): RxQuery<RxDocumentType>;
     lte(queryObj: any): RxQuery<RxDocumentType>;
     ne(queryObj: any): RxQuery<RxDocumentType>;
-    in(queryObj: any): RxQuery<RxDocumentType>;
-    nin(queryObj: any): RxQuery<RxDocumentType>;
+    in(queryObj: any[]): RxQuery<RxDocumentType>;
+    nin(queryObj: any[]): RxQuery<RxDocumentType>;
     all(queryObj: any): RxQuery<RxDocumentType>;
-    regex(queryObj: any): RxQuery<RxDocumentType>;
+    regex(queryObj: RegExp): RxQuery<RxDocumentType>;
     exists(queryObj: any): RxQuery<RxDocumentType>;
     elemMatch(queryObj: any): RxQuery<RxDocumentType>;
     sort(params: any): RxQuery<RxDocumentType>;
