d7202e6986f1e12f1d6b8599feeaa39117a3221a
Ibby Hadeed <ibby93@gmail.com>
2017-11-10 09:10:58 -0500

    Improve RxError typings

--- src/typings/rx-error.d.ts
+++ src/typings/rx-error.d.ts
@@ -1,4 +1,18 @@
+import { RxJsonSchema } from './rx-schema';
+
 export declare class RxError extends Error {
     readonly rxdb: boolean; // always true, use this to detect if its an rxdb-error
-    readonly parameters: any; // an object with parameters to use the programatically
+    readonly parameters: RxErrorParameters; // an object with parameters to use the programatically
+}
+
+export interface RxErrorParameters {
+    readonly errors: RxErrorItem[];
+    readonly schemaPath: string;
+    readonly obj: any;
+    readonly schema: RxJsonSchema;
+}
+
+export interface RxErrorItem {
+    readonly field: string;
+    readonly message: string;
 }
