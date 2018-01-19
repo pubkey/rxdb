67d454d2d1078c0772e56738ce9d8bb84c243e8a
pubkey <daniel_van_meyer@hotmail.de>
2018-01-19 21:12:58 +0100

    ADD(vue) ensure same-db-error is not thrown in dev-mode

--- examples/vue/src/database/Database.js
+++ examples/vue/src/database/Database.js
@@ -18,15 +18,32 @@ const syncURL = 'http://' + window.location.hostname + ':10101/';
 console.log('host: ' + syncURL);
 // const syncURL = host;
 
+
+/* because vue-dev-server only reloads the changed code and not the whole page,
+ * we have to ensure that the same database only exists once
+ * we can either set ignoreDuplicate to true
+ * or remove the previous instance which we do here
+ */
+window.dbs = window.dbs || [];
+const clearPrev = async function() {
+    await Promise.all(
+        window.dbs
+        .map(db => db.destroy())
+    );
+};
+
+
 let dbPromise = null;
 
 const _create = async function() {
     console.log('DatabaseService: creating database..');
+    await clearPrev();
     const db = await RxDB.create({
         name: 'heroesreactdb',
         adapter: 'idb',
         password: 'myLongAndStupidPassword'
     });
+    window.dbs.push(db);
     console.log('DatabaseService: created database');
     window['db'] = db; // write to window for debugging
 
