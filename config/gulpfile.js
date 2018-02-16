6a6a1ac4e8cf6de437ce6caf9f2725179da370d5
pubkey <daniel_van_meyer@hotmail.de>
2018-02-16 13:43:51 +0100

    CHANGE(travis) use min-reporter

--- config/gulpfile.js
+++ config/gulpfile.js
@@ -2,41 +2,43 @@ const gulp = require('gulp');
 const mocha = require('gulp-mocha');
 const tests = require('../test/unit.test');
 
+const mochaSettings = {
+    bail: true,
+    timeout: 6000,
+    exit: true,
+    reporter: 'spec'
+};
+
+if (process.env.TRAVIS) {
+    mochaSettings.timeout = 12000;
+    mochaSettings.reporter = 'min';
+}
+
 gulp.task('test', function() {
     return gulp
         .src(tests.all)
-        .pipe(mocha({
-            bail: true,
-            timeout: 6000,
-            exit: true,
-        }))
+        .pipe(mocha(mochaSettings))
         .once('end', function() {
             process.exit();
         });
 });
 
 gulp.task('profile', function() {
+    mochaSettings.bail = false;
+    mochaSettings.prof = true;
     return gulp
         .src(tests.all)
-        .pipe(mocha({
-            bail: false,
-            timeout: 6000,
-            exit: true,
-            prof: true
-        }))
+        .pipe(mocha(mochaSettings))
         .once('end', function() {
             process.exit();
         });
 });
 
 gulp.task('test:typings', function() {
+    mochaSettings.timeout = 12000;
     return gulp
         .src(tests.typings)
-        .pipe(mocha({
-            bail: true,
-            timeout: 12000,
-            exit: true
-        }))
+        .pipe(mocha(mochaSettings))
         .once('end', function() {
             process.exit();
         });
