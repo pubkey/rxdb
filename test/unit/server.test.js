28480026b9177d9f2c08268ea2525e8b0716d9fc
pubkey <daniel_van_meyer@hotmail.de>
2018-03-05 20:00:55 +0100

    ADD test

--- test/unit/server.test.js
+++ test/unit/server.test.js
@@ -10,6 +10,36 @@ import ServerPlugin from '../../plugins/server';
 RxDB.plugin(ServerPlugin);
 
 describe('server.test.js', () => {
+    it('should run and sync', async () => {
+        const serverCollection = await humansCollection.create(0);
+        await serverCollection.database.server({});
+        const clientCollection = await humansCollection.create(0);
+
+        // insert one doc on each side
+        const data1 = schemaObjects.human();
+        const data2 = schemaObjects.human();
+        await clientCollection.insert(data1);
+        await serverCollection.insert(data2);
+
+        // sync
+        clientCollection.sync({
+            remote: 'http://localhost:3000/db/human' // + serverCollection.pouch.name
+        });
+
+        await AsyncTestUtil.waitUntil(async () => {
+            const serverDocs = await serverCollection.find().exec();
+            const clientDocs = await clientCollection.find().exec();
+            console.log('serverDocs: ' + serverDocs.length);
+            console.log('clientDocs: ' + clientDocs.length);
+            console.dir(clientDocs.map(doc => doc.toJSON()));
+            return (clientDocs.length === 2 && serverDocs.length === 2);
+        });
+
+        clientCollection.database.destroy();
+        serverCollection.database.destroy();
+
+        process.exit();
+    });
     it('run', async function() {
         this.timeout(200 * 1000);
 
