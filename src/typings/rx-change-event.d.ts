43e289af0c6b30535a0a7c59bee3870a3be6c947
pubkey <daniel_van_meyer@hotmail.de>
2018-04-23 17:19:19 +0200

    ADD(rx-change-event) better typings

--- src/typings/rx-change-event.d.ts
+++ src/typings/rx-change-event.d.ts
@@ -1,17 +1,25 @@
-export type RxChangeEventOperation = 'INSERT' | 'UPDATE' | 'REMOVE'
+export type RxChangeEventOperation =
+    'INSERT' | // document created
+    'UPDATE' | // document changed
+    'REMOVE' | // document removed
+    'RxDatabase.collection'; // collection created
 
 export interface RxChangeEventData<T = {}> {
-  readonly col?: string;
-  readonly db: string;
-  readonly doc?: string;
-  readonly isLocal?: boolean;
-  readonly it: string;
-  readonly op: RxChangeEventOperation;
-  readonly t: number;
-  readonly v?: T;
+    readonly col?: string;
+    readonly db: string;
+    readonly doc?: string;
+    readonly isLocal?: boolean;
+    readonly it: string;
+    readonly op: RxChangeEventOperation;
+    readonly t: number;
+    readonly v?: T;
 }
 
 export declare class RxChangeEvent<T = {}> {
     data: RxChangeEventData<T>;
     toJSON(): RxChangeEventData<T>;
+
+    isIntern(): boolean;
+    isSocket(): boolean;
+    hash: string;
 }
