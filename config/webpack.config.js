41cb52e490194deda2f8da1cc2e6aafc5708002b
pubkey <daniel_van_meyer@hotmail.de>
2018-07-10 18:17:54 +0200

    CHANGE measure build-size with webpack instead of browserify

---
+++ config/webpack.config.js
@@ -0,0 +1,28 @@
+
+const path = require('path');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+
+
+//console.log(process.env.NODE_ENV);
+//process.exit();
+
+
+const plugins = [];
+if (process.env.NODE_ENV === 'disc')
+    plugins.push(new BundleAnalyzerPlugin());
+
+module.exports = {
+    mode: 'production',
+    entry: './dist/es/index.js',
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin()
+        ]
+    },
+    plugins,
+    output: {
+        path: path.resolve(__dirname, '../test_tmp'),
+        filename: 'webpack.bundle.js'
+    }
+};
