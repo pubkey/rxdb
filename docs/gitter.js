64624f07253817c7cef0e1b71f743d98cc7fee8a
pubkey <daniel_van_meyer@hotmail.de>
2019-02-21 02:54:55 +0100

    FIX gitter plugin

--- docs/gitter.js
+++ docs/gitter.js
@@ -1,10 +1,10 @@
 console.log('load gitter widget');
+((window.gitter = {}).chat = {}).options = {
+    room: 'pubkey/rxdb'
+};
+console.dir(window.gitter);
 var script = document.createElement('script');
 script.src = 'https://sidecar.gitter.im/dist/sidecar.v1.js';
 script.onload = function () {
-    ((window.gitter = {}).chat = {}).options = {
-        room: 'pubkey/rxdb'
-    };
-    console.dir(window.gitter);
 };
 document.head.appendChild(script);
