44795604c8f471a61a8c18c8dd5a8bb8d0d5fdf9
Gautam BT <git-author@l1cache.in>
2020-03-22 12:37:39 +0530

    GraphQL Replication Plugin: Add syncRevisions to SyncOptionsGraphQL type

--- src/types/plugins/replication-graphql.ts
+++ src/types/plugins/replication-graphql.ts
@@ -24,4 +24,5 @@ export type SyncOptionsGraphQL = {
     liveInterval?: number; // time in ms
     retryTime?: number; // time in ms
     autoStart?: boolean; // if this is false, the replication does nothing at start
+    syncRevisions?: boolean;
 };
