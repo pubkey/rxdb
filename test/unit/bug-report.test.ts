59c8b85fdd5ec67345125bc9246f17b160728989
maurice <st147119@stud.uni-stuttgart.de>
2020-05-12 12:30:34 +0200

    change bug-report.test.ts (possible revision tree/document collision bug)

--- test/unit/bug-report.test.ts
+++ test/unit/bug-report.test.ts
@@ -2,42 +2,40 @@
  * this is a template for a test.
  * If you found a bug, edit this test to reproduce it
  * and than make a pull-request with that failing test.
- * The maintainer will later move your test to the correct possition in the test-suite.
+ * The maintainer will later move your test to the correct position in the test-suite.
  *
  * To run this test do:
  * - 'npm run test:node' so it runs in nodejs
  * - 'npm run test:browsers' so it runs in the browser
  */
 import assert from 'assert';
-import AsyncTestUtil from 'async-test-util';
 
 import {
     createRxDatabase, randomCouchString
 } from '../../';
+import {RxJsonSchema} from '../../src';
 
 describe('bug-report.test.js', () => {
     it('should fail because it reproduces the bug', async () => {
         // create a schema
-        const mySchema = {
+        class Item {
+            id: number = -1;
+            projectId: string = '';
+            name: string = '';
+        }
+
+        const mySchema: RxJsonSchema<Item> = {
+            title: '',
+            description: '',
             version: 0,
+            keyCompression: false,
             type: 'object',
             properties: {
-                passportId: {
-                    type: 'string',
-                    primary: true
-                },
-                firstName: {
-                    type: 'string'
-                },
-                lastName: {
-                    type: 'string'
-                },
-                age: {
-                    type: 'integer',
-                    minimum: 0,
-                    maximum: 150
-                }
-            }
+                projectId: { type: 'string' },
+                id: { type: 'number' },
+                name: { type: 'string' },
+            },
+            required: ['projectId', 'id', 'name'],
         };
 
         // generate a random database-name
@@ -56,54 +54,102 @@ describe('bug-report.test.js', () => {
             schema: mySchema
         });
 
+
+        const item = new Item();
+        item.projectId = '...';
+        item.id = 1;
+        item.name = 'spasd';
+
         // insert a document
-        await collection.insert({
-            passportId: 'foobar',
-            firstName: 'Bob',
-            lastName: 'Kelso',
-            age: 56
-        });
-
-        /**
-         * to simulate the event-propagation over multiple browser-tabs,
-         * we create the same database again
-         */
-        const dbInOtherTab = await createRxDatabase({
-            name,
-            adapter: 'memory',
-            eventReduce: true,
-            ignoreDuplicate: true
-        });
-        // create a collection
-        const collectionInOtherTab = await dbInOtherTab.collection({
-            name: 'mycollection',
-            schema: mySchema
-        });
-
-        // find the document in the other tab
-        const myDocument = await collectionInOtherTab
-            .findOne()
-            .where('firstName')
-            .eq('Bob')
-            .exec();
+        await collection.insert(item);
 
         /*
          * assert things,
          * here your tests should fail to show that there is a bug
          */
-        assert.strictEqual(myDocument.age, 56);
-
-        // you can also wait for events
-        const emitted = [];
-        const sub = collectionInOtherTab
-            .findOne().$
-            .subscribe(doc => emitted.push(doc));
-        await AsyncTestUtil.waitUntil(() => emitted.length === 1);
-
+        assert.doesNotThrow(await collection
+            .findOne()
+            .where('projectId')
+            .eq('...')
+            .where('id')
+            .eq(1)
+            .update({ $set: { name: 'new name' } }));
 
         // clean up afterwards
-        sub.unsubscribe();
         db.destroy();
-        dbInOtherTab.destroy();
     });
 });
+
+// npm run test:node
+// 1) bug-report.test.js
+// should fail because it reproduces the bug:
+//     RxError (VD2): RxError:
+//     object does not match schema
+// Given parameters: {
+//     errors:[
+//         {
+//             "field": "data",
+//             "message": "has additional properties"
+//         }
+//     ]
+//     obj:{
+//         "id": 1,
+//             "projectId": "...",
+//             "name": "new name",
+//             "_id": "yb4nfgu7cb:1589224338163",
+//             "_rev_tree": [
+//             {
+//                 "pos": 1,
+//                 "ids": [
+//                     "14bf68a361ba67f4ef3fed697822106c",
+//                     {
+//                         "status": "available"
+//                     },
+//                     []
+//                 ]
+//             }
+//         ],
+//             "_rev": "1-14bf68a361ba67f4ef3fed697822106c"
+//     }
+//     schema:{
+//         "title": "",
+//             "description": "",
+//             "version": 0,
+//             "keyCompression": false,
+//             "type": "object",
+//             "properties": {
+//             "projectId": {
+//                 "type": "string"
+//             },
+//             "id": {
+//                 "type": "number"
+//             },
+//             "name": {
+//                 "type": "string"
+//             },
+//             "_rev": {
+//                 "type": "string",
+//                     "minLength": 1
+//             },
+//             "_attachments": {
+//                 "type": "object"
+//             },
+//             "_id": {
+//                 "type": "string",
+//                     "minLength": 1
+//             }
+//         },
+//         "required": [
+//             "projectId",
+//             "id",
+//             "name",
+//             "_id"
+//         ],
+//             "additionalProperties": false,
+//             "indexes": [],
+//             "encrypted": []
+//     }}
+// at newRxError (src/rx-error.ts:104:12)
+// at RxSchema.validate (src/plugins/validate.ts:52:15)
+// at /home/maurice/Documents/common/projects/rxdb/src/rx-document.ts:314:40
+
