69c85a25ca1a485a3f8beb41487c7da0a9bb783d
DDoerner <nik_doerner@web.de>
2020-10-01 18:01:54 +0200

    Allow to skip docs in gql replication #2552

--- src/plugins/replication-graphql/index.ts
+++ src/plugins/replication-graphql/index.ts
@@ -222,8 +222,11 @@ export class RxGraphQLReplicationState {
 
         // this assumes that there will be always only one property in the response
         // is this correct?
-        const data = result.data[Object.keys(result.data)[0]];
-        const modified: any[] = await Promise.all(data.map(async (doc: any) => await (this.pull as any).modifier(doc)));
+        const data: any[] = result.data[Object.keys(result.data)[0]];
+        const modified: any[] = await Promise.all(data
+            .map(async (doc: any) => await (this.pull as any).modifier(doc))
+            .filter(doc => doc)
+        );
 
 
         /**
@@ -286,7 +289,7 @@ export class RxGraphQLReplicationState {
             this.syncRevisions
         );
 
-        const changesWithDocs = await Promise.all(changes.results.map(async (change: any) => {
+        const changesWithDocs: any = await Promise.all(changes.results.map(async (change: any) => {
             let doc = change['doc'];
 
             doc[this.deletedFlag] = !!change['deleted'];
@@ -299,13 +302,16 @@ export class RxGraphQLReplicationState {
             }
 
             doc = await (this.push as any).modifier(doc);
+            if (!doc) {
+                return null;
+            }
 
             const seq = change.seq;
             return {
                 doc,
                 seq
             };
-        }));
+        }).filter(doc => doc));
 
         let lastSuccessfullChange = null;
         try {
