52f08086867abb03a654f1dd5fa6d4816295346e
pubkey <8926560+pubkey@users.noreply.github.com>
2021-06-20 21:56:05 +0200

    FIX ci

--- src/rx-collection.ts
+++ src/rx-collection.ts
@@ -553,7 +553,13 @@ export class RxCollectionBase<
             .then((wasInserted: any) => {
                 if (!wasInserted.inserted) {
                     return _atomicUpsertUpdate(wasInserted.doc, json)
-                        .then(() => nextTick()) // tick here so the event can propagate
+                        /**
+                         * tick here so the event can propagate
+                         * TODO we should not need that here
+                         */
+                        .then(() => nextTick())
+                        .then(() => nextTick())
+                        .then(() => nextTick())
                         .then(() => wasInserted.doc);
                 } else
                     return wasInserted.doc;
