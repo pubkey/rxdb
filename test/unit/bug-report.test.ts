1da573db356543308b64af20d022d640b4e35f6a
Brandon <brobertstech@gmail.com>
2021-09-17 10:41:35 -0700

    Add breaking test to confirm local doc is not deleted on removeRxDatabase

--- test/unit/bug-report.test.ts
+++ test/unit/bug-report.test.ts
@@ -1,118 +1,40 @@
-/**
- * this is a template for a test.
- * If you found a bug, edit this test to reproduce it
- * and than make a pull-request with that failing test.
- * The maintainer will later move your test to the correct possition in the test-suite.
- *
- * To run this test do:
- * - 'npm run test:node' so it runs in nodejs
- * - 'npm run test:browser' so it runs in the browser
- */
 import assert from 'assert';
-import AsyncTestUtil from 'async-test-util';
 
-import {
-    createRxDatabase,
-    randomCouchString
-} from '../../plugins/core';
+import * as humansCollection from '../helper/humans-collection';
 
-import {
-    getRxStoragePouch,
-} from '../../plugins/pouchdb';
+import { addRxPlugin } from '../../plugins/core';
 
+import { RxDBLocalDocumentsPlugin } from '../../plugins/local-documents';
+addRxPlugin(RxDBLocalDocumentsPlugin);
+import config from './config';
+import { removeRxDatabase } from '../../src/rx-database';
 
-describe('bug-report.test.js', () => {
-    it('should fail because it reproduces the bug', async () => {
+let leveldown: any;
+if (config.platform.isNode()) {
+    leveldown = require('leveldown');
+}
 
+declare type TestDocType = {
+    foo: string;
+};
 
-        // create a schema
-        const mySchema = {
-            version: 0,
-            primaryKey: 'passportId',
-            type: 'object',
-            properties: {
-                passportId: {
-                    type: 'string'
-                },
-                firstName: {
-                    type: 'string'
-                },
-                lastName: {
-                    type: 'string'
-                },
-                age: {
-                    type: 'integer',
-                    minimum: 0,
-                    maximum: 150
-                }
-            }
-        };
+config.parallel('bug-report.test.ts', () => {
+    describe('removeRxDB()!!!', () => {
+        it('should delete local docs ', async () => {
+            const localDocId = 'foobar';
+            const c = await humansCollection.create();
+            const doc = await c.insertLocal(localDocId, { foo: 'bar' });
 
-        // generate a random database-name
-        const name = randomCouchString(10);
+            // remove the database
+            await c.database.remove();
 
-        // create a database
-        const db = await createRxDatabase({
-            name,
-            storage: getRxStoragePouch('memory'),
-            eventReduce: true,
-            ignoreDuplicate: true
+            // create a brand new database
+            const cNew = await humansCollection.create();
+
+            // get local doc from first database
+            const fooBar = await c.getLocal(localDocId);
+
+            assert.strictEqual(fooBar, undefined);
         });
-        // create a collection
-        const collections = await db.addCollections({
-            mycollection: {
-                schema: mySchema
-            }
-        });
-
-        // insert a document
-        await collections.mycollection.insert({
-            passportId: 'foobar',
-            firstName: 'Bob',
-            lastName: 'Kelso',
-            age: 56
-        });
-
-        /**
-         * to simulate the event-propagation over multiple browser-tabs,
-         * we create the same database again
-         */
-        const dbInOtherTab = await createRxDatabase({
-            name,
-            storage: getRxStoragePouch('memory'),
-            eventReduce: true,
-            ignoreDuplicate: true
-        });
-        // create a collection
-        const collectionInOtherTab = await dbInOtherTab.addCollections({
-            mycollection: {
-                schema: mySchema
-            }
-        });
-
-        // find the document in the other tab
-        const myDocument = await collectionInOtherTab.mycollection
-            .findOne()
-            .where('firstName')
-            .eq('Bob')
-            .exec();
-
-        /*
-         * assert things,
-         * here your tests should fail to show that there is a bug
-         */
-        assert.strictEqual(myDocument.age, 56);
-
-        // you can also wait for events
-        const emitted = [];
-        const sub = collectionInOtherTab.mycollection
-            .findOne().$
-            .subscribe(doc => emitted.push(doc));
-        await AsyncTestUtil.waitUntil(() => emitted.length === 1);
-
-        // clean up afterwards
-        sub.unsubscribe();
-        db.destroy();
-        dbInOtherTab.destroy();
     });
 });
