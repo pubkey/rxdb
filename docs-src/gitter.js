ee6dd5d70d2b995008262fcd9aa6a0f61f4b51db
pubkey <8926560+pubkey@users.noreply.github.com>
2022-01-14 19:16:24 +0100

    ADD(landingpage) better mobile

--- docs-src/gitter.js
+++ docs-src/gitter.js
@@ -8,3 +8,11 @@ script.src = 'https://sidecar.gitter.im/dist/sidecar.v1.js';
 script.onload = function () {
 };
 document.head.appendChild(script);
+
+
+
+// overwrite some css rules
+var styleSheet = document.createElement('style');
+styleSheet.type = 'text/css';
+styleSheet.innerText = '.gitter-open-chat-button {background-color: #e6008d !important; font-weight: bold;}';
+document.head.appendChild(styleSheet);
