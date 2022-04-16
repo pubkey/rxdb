071c26b4258020dddd0e6c1fe8aead1c28e12560
pubkey <8926560+pubkey@users.noreply.github.com>
2022-04-16 13:47:32 +0200

    REMOVE not need file

--- src/types/modules/binary-search-bounds.d.ts
+++
@@ -1,7 +0,0 @@
-/**
- * Because the typings of this module are broken,
- * we have to monkey patch it.
- * TODO remove this if the fix has been merged:
- * @link https://github.com/mikolalysenko/binary-search-bounds/pull/12
- */
-declare module 'binary-search-bounds';
