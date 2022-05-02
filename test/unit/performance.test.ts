de4538440e4086b78342479680d9403a047e6263
pubkey <8926560+pubkey@users.noreply.github.com>
2022-05-02 15:03:32 +0200

    FIX perf test

--- test/unit/performance.test.ts
+++ test/unit/performance.test.ts
@@ -14,102 +14,105 @@ import config from './config';
  * was the easiest way to make it run on all storages and platforms.
  * Maybe we should move this into a different npm script instead.
  */
-describe('unit/performance.test.ts', async function () {
-    if (config.isFastMode()) {
-        return;
-    }
-    this.timeout(90 * 1000);
+describe('unit/performance.test.ts', () => {
+    it('run the performance test', async function () {
 
-    const perfStorage = config.storage.getPerformanceStorage();
+        if (config.isFastMode()) {
+            return;
+        }
+        this.timeout(90 * 1000);
 
-    const totalTimeSums: { [k: string]: number } = {};
+        const perfStorage = config.storage.getPerformanceStorage();
 
-    const runs = 10;
-    let runsDone = 0;
-    while (runsDone < runs) {
-        runsDone++;
+        const totalTimeSums: { [k: string]: number } = {};
 
-        let time = performance.now();
-        const updateTime = (flag?: string) => {
-            if (!flag) {
+        const runs = 10;
+        let runsDone = 0;
+        while (runsDone < runs) {
+            runsDone++;
+
+            let time = performance.now();
+            const updateTime = (flag?: string) => {
+                if (!flag) {
+                    time = performance.now();
+                    return;
+                }
+                const diff = performance.now() - time;
+                if (!totalTimeSums[flag]) {
+                    totalTimeSums[flag] = diff;
+                } else {
+                    totalTimeSums[flag] = totalTimeSums[flag] + diff;
+                }
                 time = performance.now();
-                return;
             }
-            const diff = performance.now() - time;
-            if (!totalTimeSums[flag]) {
-                totalTimeSums[flag] = diff;
-            } else {
-                totalTimeSums[flag] = totalTimeSums[flag] + diff;
-            }
-            time = performance.now();
+
+            // create database
+            const db = await createRxDatabase({
+                name: randomCouchString(10),
+                eventReduce: true,
+                /**
+                 * A RxStorage implementation (like LokiJS)
+                 * might need a full leader election cycle to be useable.
+                 * So we disable multiInstance here because it would make no sense
+                 * to measure the leader election time instead of the database
+                 * creation time.
+                 */
+                multiInstance: false,
+                storage: perfStorage.storage
+            });
+            updateTime('database-creation');
+
+            // create collections
+            const collectionData: any = {};
+            new Array(8)
+                .fill(0)
+                .forEach((_v, idx) => {
+                    const name = 'human' + idx;
+                    collectionData[name] = {
+                        schema: schemas.averageSchema(),
+                        statics: {}
+                    };
+                });
+            await db.addCollections(collectionData);
+            updateTime('collection-creation');
+
+            // insert documents
+            const docIds: string[] = [];
+            const docsData = new Array(100)
+                .fill(0)
+                .map(() => {
+                    const data = schemaObjects.averageSchema();
+                    docIds.push(data.id);
+                    return data;
+                });
+            updateTime();
+            await db.human0.bulkInsert(docsData);
+            updateTime('insert-documents');
+
+            // find by id
+            await db.human0.findByIds(docIds);
+            updateTime('find-by-ids');
+
+            // find by query
+            await db.human0.find({
+                selector: {
+                    var1: {
+                        $gt: ''
+                    }
+                }
+            }).exec();
+            updateTime('find-by-query');
+
+            await db.destroy();
         }
 
-        // create database
-        const db = await createRxDatabase({
-            name: randomCouchString(10),
-            eventReduce: true,
-            /**
-             * A RxStorage implementation (like LokiJS)
-             * might need a full leader election cycle to be useable.
-             * So we disable multiInstance here because it would make no sense
-             * to measure the leader election time instead of the database
-             * creation time.
-             */
-            multiInstance: false,
-            storage: perfStorage.storage
+
+        const timeToLog: any = {};
+        Object.entries(totalTimeSums).forEach(([key, totalTime]) => {
+            timeToLog[key] = totalTime / runs;
         });
-        updateTime('database-creation');
 
-        // create collections
-        const collectionData: any = {};
-        new Array(8)
-            .fill(0)
-            .forEach((_v, idx) => {
-                const name = 'human' + idx;
-                collectionData[name] = {
-                    schema: schemas.averageSchema(),
-                    statics: {}
-                };
-            });
-        await db.addCollections(collectionData);
-        updateTime('collection-creation');
-
-        // insert documents
-        const docIds: string[] = [];
-        const docsData = new Array(100)
-            .fill(0)
-            .map(() => {
-                const data = schemaObjects.averageSchema();
-                docIds.push(data.id);
-                return data;
-            });
-        updateTime();
-        await db.human0.bulkInsert(docsData);
-        updateTime('insert-documents');
-
-        // find by id
-        await db.human0.findByIds(docIds);
-        updateTime('find-by-ids');
-
-        // find by query
-        await db.human0.find({
-            selector: {
-                var1: {
-                    $gt: ''
-                }
-            }
-        }).exec();
-        updateTime('find-by-query');
-
-        await db.destroy();
-    }
-
-
-    const timeToLog: any = {};
-    Object.entries(totalTimeSums).forEach(([key, totalTime]) => {
-        timeToLog[key] = totalTime / runs;
+        console.log('Performance test for ' + perfStorage.description);
+        console.log(JSON.stringify(timeToLog, null, 4));
     });
-
-    console.log('Performance test for ' + perfStorage.description);
-    console.log(JSON.stringify(timeToLog, null, 4));
 });
