c711badc92e2f98e036d71fc7a770d1a3d9e4c82
Ivan Buryak <buryak.ivan@yandex.ru>
2022-05-09 10:06:23 +0300

    getPersistendStorage in humanscollection

--- test/helper/humans-collection.ts
+++ test/helper/humans-collection.ts
@@ -455,7 +455,7 @@ export async function createMigrationCollection(
     const colName = 'human';
     const db = await createRxDatabase<{ human: RxCollection<schemaObjects.SimpleHumanAgeDocumentType> }>({
         name,
-        storage: config.storage.getStorage(),
+        storage: config.storage.getPersistendStorage(),
         eventReduce: true,
         ignoreDuplicate: true
     });
@@ -481,7 +481,7 @@ export async function createMigrationCollection(
 
     const db2 = await createRxDatabase<{ human: RxCollection<schemaObjects.SimpleHumanV3DocumentType> }>({
         name,
-        storage: config.storage.getStorage(),
+        storage: config.storage.getPersistendStorage(),
         eventReduce: true,
         ignoreDuplicate: true
     });
