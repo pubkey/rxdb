657b901ad2c300f2c05b4e3afe8cee3125426451
pubkey <8926560+pubkey@users.noreply.github.com>
2022-05-10 21:17:01 +0200

    REMOVE logs

--- src/plugins/dexie/dexie-helper.ts
+++ src/plugins/dexie/dexie-helper.ts
@@ -104,7 +104,6 @@ export function getDexieSortComparator<RxDocType>(
         [fieldName: string]: 1 | -1;
     } = {};
 
-    console.dir(query);
     if (!query.sort) {
         throw newRxError('SNH', { query });
     }
