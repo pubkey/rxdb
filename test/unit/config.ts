564b2e6a0e75a302cfb0c6ba132a892d36f4c1ef
pubkey <8926560+pubkey@users.noreply.github.com>
2022-07-20 03:56:05 +0200

    ADD run test suite once with validation

--- test/unit/config.ts
+++ test/unit/config.ts
@@ -14,6 +14,7 @@ import { getRxStorageDexie, RxStorageDexieStatics } from '../../plugins/dexie';
 import { getRxStorageWorker } from '../../plugins/worker';
 import { getRxStorageMemory } from '../../plugins/memory';
 import { CUSTOM_STORAGE } from './custom-storage';
+import { wrappedValidateIsMyJsonValidStorage } from '../../plugins/validate-is-my-json-valid';
 
 
 const ENV_VARIABLES = detect().name === 'node' ? process.env : (window as any).__karma__.config.env;
@@ -101,6 +102,31 @@ export function setDefaultStorage(storageKey: string) {
                     return {
                         description: 'memory',
                         storage: getRxStorageMemory()
+                    }
+                },
+                hasPersistence: false,
+                hasMultiInstance: false,
+                hasCouchDBReplication: false,
+                hasAttachments: true,
+                hasRegexSupport: true
+            };
+            break;
+            /**
+             * We run the tests once together
+             * with a validation plugin
+             * to ensure we do not accidentially use non-valid data
+             * in the tests.
+             */
+        case 'memory-validation':
+            config.storage = {
+                name: 'memory',
+                getStorage: () => getRxStorageMemory(),
+                getPerformanceStorage() {
+                    return {
+                        description: 'memory',
+                        storage: wrappedValidateIsMyJsonValidStorage({
+                            storage: getRxStorageMemory()
+                        })
                     }
                 },
                 hasPersistence: false,
