23c7ddf393ea7c66ca5dcdef2691b3fc155caf6f
msotnikov <mishichka@gmail.com>
2022-09-05 20:30:33 +0300

    fix(): jest bugs

--- examples/react-native/jest.config.js
+++ examples/react-native/jest.config.js
@@ -7,8 +7,6 @@ module.exports = {
         },
     },
     testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-    cacheDirectory: '.jest/cache',
-    testEnvironment: 'jsdom',
     transformIgnorePatterns: [
         "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|pouchdb-adapter.*)"
     ],
