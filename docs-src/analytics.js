39952ad090e9f8a661645f221042998ea94b0b29
pubkey <8926560+pubkey@users.noreply.github.com>
2022-10-06 14:58:25 +0200

    FIX reddit pixel

--- docs-src/analytics.js
+++ docs-src/analytics.js
@@ -12,3 +12,27 @@ function gtag() {
 gtag('js', new Date());
 
 gtag('config', 'G-62D63SY3S0');
+
+
+
+
+
+// Reddit Pixel
+! function (w, d) {
+    if (!w.rdt) {
+        var p = w.rdt = function () {
+            p.sendEvent ? p.sendEvent.apply(p, arguments) : p.callQueue.push(arguments)
+        };
+        p.callQueue = [];
+        var t = d.createElement('script');
+        t.src = 'https://www.redditstatic.com/ads/pixel.js', t.async = !0;
+        var s = d.getElementsByTagName('script')[0];
+        s.parentNode.insertBefore(t, s)
+    }
+}(window, document);
+window.rdt('init', 't2_131k54', {
+    optOut: false,
+    useDecimalCurrencyValues: true
+});
+window.rdt('track', 'PageVisit');
+// /Reddit Pixel
