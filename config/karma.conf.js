d0d0a0635c44081454c03e298c332490dc21f6b8
AlbertGao <albertgaohy@gmail.com>
2022-10-14 09:41:47 +0100

    Ignored SafariTechPreview in test (#4058)

--- config/karma.conf.js
+++ config/karma.conf.js
@@ -83,6 +83,7 @@ const configuration = {
 
             const doNotUseTheseBrowsers = [
                 'PhantomJS',
+                'SafariTechPreview',
                 'FirefoxAurora',
                 'FirefoxNightly',
                 'ChromeCanary'
