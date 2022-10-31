3eecf43db23bec8157b31375f4e92e6b1aaea58f
pubkey <8926560+pubkey@users.noreply.github.com>
2022-10-31 01:09:24 +0100

    ADD foundationdb count

--- src/plugins/foundationdb/foundationdb-query.ts
+++ src/plugins/foundationdb/foundationdb-query.ts
@@ -5,6 +5,7 @@ import {
 } from '../../custom-index';
 import type {
     RxDocumentData,
+    RxStorageCountResult,
     RxStorageQueryResult
 } from '../../types';
 import { ensureNotFalsy } from '../../util';
