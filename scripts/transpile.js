4469eb87f7f1eb8db6c210bdb942f8cc943bfc5a
pubkey <8926560+pubkey@users.noreply.github.com>
2022-11-06 17:15:29 +0100

    FIX incremental transpile also es6 so that webpack browser tests know about the changes

--- scripts/transpile.js
+++ scripts/transpile.js
@@ -24,10 +24,21 @@ const DEBUG = false;
 /**
  * key->value | src -> compiled
  */
-const transpileFolders = {
-    'src': 'dist/lib',
-    'test': 'test_tmp'
-};
+const TRANSPILE_FOLDERS = [
+    {
+        source: 'src',
+        goals: {
+            es5: 'dist/lib',
+            es6: 'dist/es'
+        }
+    },
+    {
+        source: 'test',
+        goals: {
+            es5: 'test_tmp'
+        }
+    }
+]
 
 nconf.argv()
     .env()
@@ -35,7 +46,11 @@ nconf.argv()
         file: confLocation
     });
 
-async function transpileFile(srcLocations, outDir) {
+async function transpileFile(
+    srcLocations,
+    outDir,
+    env
+) {
     DEBUG && console.log('transpile: ' + srcLocations.join(', '));
     // ensure folder exists
     const folder = path.join(outDir);
@@ -45,7 +60,8 @@ async function transpileFile(srcLocations, outDir) {
 
     // const outFilePath = 
     // await del.promise([outDir]);
-    const cmd = 'babel ' +
+    const cmd = 'cross-env NODE_ENV=' + env +
+        ' babel ' +
         srcLocations.join(' ') +
         ' --source-maps' +
         ' --extensions ".ts,.js"' +
@@ -75,11 +91,17 @@ async function transpileFile(srcLocations, outDir) {
 async function getFiles() {
     const files = [];
     await Promise.all(
-        Object.entries(transpileFolders)
-            .map(entry => entry.map(folder => path.join(basePath, folder)))
-            .map(entry => {
-                const srcFolder = entry[0];
-                const toFolder = entry[1];
+        TRANSPILE_FOLDERS
+            // make all file paths absolute
+            .map(transpileFolder => {
+                transpileFolder.source = path.join(basePath, transpileFolder.source);
+                Object.entries(transpileFolder.goals).forEach(([key, value]) => {
+                    transpileFolder.goals[key] = path.join(basePath, value);
+                });
+                return transpileFolder;
+            })
+            .map(transpileFolder => {
+                const srcFolder = transpileFolder.source;
                 return walkSync.entries(srcFolder)
                     .filter(entry => !entry.isDirectory())
                     .filter(entry => entry.relativePath.endsWith('.js') || entry.relativePath.endsWith('.ts'))
@@ -89,27 +111,27 @@ async function getFiles() {
                         const relativePathSplit = fileEntry.relativePath.split('.');
                         relativePathSplit.pop();
                         relativePathSplit.push('js');
+                        Object.entries(transpileFolder.goals).forEach(([env, toFolder]) => {
+                            const goalPath = path.join(toFolder, relativePathSplit.join('.'));
+                            const fullPath = path.join(fileEntry.basePath, fileEntry.relativePath);
 
-                        fileEntry.goalFolder = toFolder;
-                        const goalPath = path.join(toFolder, relativePathSplit.join('.'));
-                        const fullPath = path.join(fileEntry.basePath, fileEntry.relativePath);
-
-                        const file = {
-                            fullPath,
-                            relativePath: fileEntry.relativePath,
-                            basePath: fileEntry.basePath,
-                            mtime: fileEntry.mtime,
-                            goalFolder: path.dirname(goalPath),
-                            goalPath: goalPath,
-                        };
-
-                        const lastTime = parseInt(nconf.get(fileEntry.fullPath), 10);
-                        if (
-                            lastTime !== fileEntry.mtime ||
-                            !existsFile.sync(goalPath)
-                        ) {
-                            files.push(file);
-                        }
+                            const lastTime = parseInt(nconf.get(fileEntry.fullPath), 10);
+                            const file = {
+                                env,
+                                fullPath,
+                                relativePath: fileEntry.relativePath,
+                                basePath: fileEntry.basePath,
+                                mtime: fileEntry.mtime,
+                                goalFolder: path.dirname(goalPath),
+                                goalPath: goalPath,
+                            };
+                            if (
+                                lastTime !== fileEntry.mtime ||
+                                !existsFile.sync(goalPath)
+                            ) {
+                                files.push(file);
+                            }
+                        });
                     });
             })
     );
@@ -133,11 +155,28 @@ async function run() {
 
     await Promise.all(
         Object.values(files).map(async (filesWithSameGoalFolder) => {
-            await transpileFile(
-                filesWithSameGoalFolder.map(file => path.join(file.basePath, file.relativePath)),
-                filesWithSameGoalFolder[0].goalFolder
+
+            const byEnv = {};
+            filesWithSameGoalFolder.forEach(row => {
+                const env = row.env;
+                if (!byEnv[env]) {
+                    byEnv[env] = [];
+                }
+                const ar = byEnv[env];
+                ar.push(row);
+            });
+
+            await Promise.all(
+                Object.entries(byEnv)
+                    .map(async ([env, files]) => {
+                        await transpileFile(
+                            files.map(file => path.join(file.basePath, file.relativePath)),
+                            files[0].goalFolder,
+                            env
+                        );
+                        files.forEach(file => nconf.set(file.fullPath, file.mtime));
+                    })
             );
-            filesWithSameGoalFolder.forEach(file => nconf.set(file.fullPath, file.mtime));
         })
     );
 
