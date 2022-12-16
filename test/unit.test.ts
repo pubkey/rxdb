37692a68ee25e77d91ae8ccbf50f99e0e9d347fd
pubkey <8926560+pubkey@users.noreply.github.com>
2022-12-16 16:21:17 +0100

    FIX imports

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -55,7 +55,6 @@ import './unit/replication.test';
 import './unit/replication-graphql.test';
 import './unit/replication-couchdb-new.test';
 import './unit/replication-websocket.test';
-import './unit/replication-couchdb.test';
 import './unit/replication-p2p.test';
 import './unit/crdt.test';
 import './unit/server-couchdb.test';
