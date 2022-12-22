d2c8fe45dacbdf161068a435af166718e27a8aab
Sebastian Guevara <info@sebasg.dev>
2022-12-21 23:07:01 -0500

    misc(examples): fix lint

--- examples/vite-vanilla-ts/src/schema.ts
+++ examples/vite-vanilla-ts/src/schema.ts
@@ -3,40 +3,39 @@ import {
     type ExtractDocumentTypeFromTypedRxJsonSchema,
     type RxCollection,
     type RxDatabase,
-    type RxJsonSchema
+    type RxJsonSchema,
 } from 'rxdb';
 
 export const NotesSchemaLiteral = {
-   title: 'note',
-  description: 'an individual note',
-  version: 0,
-  type: 'object',
-  indexes: [
-    'createdAt',
-    'updatedAt'
-  ],
-  primaryKey: 'name',
-  properties: {
-    name: {
-      type: 'string',
+    title: 'note',
+    description: 'an individual note',
+    version: 0,
+    type: 'object',
+    indexes: ['createdAt', 'updatedAt'],
+    primaryKey: 'name',
+    properties: {
+        name: {
+            type: 'string',
+        },
+        body: {
+            type: 'string',
+        },
+        createdAt: {
+            type: 'number',
+        },
+        updatedAt: {
+            type: 'number',
+        },
     },
-    body: {
-      type: 'string'
-    },
-    createdAt: {
-      type: 'number',
-    },
-    updatedAt: {
-      type: 'number',
-    },
-  },
-  required: ['name'],
+    required: ['name', 'body', 'createdAt', 'updatedAt'],
 } as const;
 
 export const SchemaTyped = toTypedRxJsonSchema(NotesSchemaLiteral);
 
 // aggregate the document type from the schema
-export type NoteDocType = ExtractDocumentTypeFromTypedRxJsonSchema<typeof SchemaTyped>;
+export type NoteDocType = ExtractDocumentTypeFromTypedRxJsonSchema<
+    typeof SchemaTyped
+>;
 
 // create the typed RxJsonSchema from the literal typed object.
 export const NoteSchema: RxJsonSchema<NoteDocType> = SchemaTyped;
@@ -44,7 +43,7 @@ export const NoteSchema: RxJsonSchema<NoteDocType> = SchemaTyped;
 type NoteCollection = RxCollection<NoteDocType>;
 
 export type MyDatabaseCollections = {
-notes: NoteCollection;
+    notes: NoteCollection;
 };
 
 export type MyDatabase = RxDatabase<MyDatabaseCollections>;
