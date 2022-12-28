ab635216ff9d824e1cec76a15d90eb83321cda00
pubkey <8926560+pubkey@users.noreply.github.com>
2022-12-28 01:11:21 +0100

    FIX lint

--- src/rx-query-mingo.ts
+++ src/rx-query-mingo.ts
@@ -29,7 +29,7 @@ let mingoInitDone = false;
  * The MongoDB query library is huge and we do not need all the operators.
  * If you add an operator here, make sure that you properly add a test in
  * the file /test/unit/rx-storage-query-correctness.test.ts
- * 
+ *
  * @link https://github.com/kofrasa/mingo#es6
  */
 export function getMingoQuery<RxDocType>(
