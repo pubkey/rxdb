8c89d4e2ae18176698d42b7f9b8dd23b98c9d816
pubkey <8926560+pubkey@users.noreply.github.com>
2023-01-30 14:47:23 +0100

    FIX mingo tree shaking

--- src/rx-query-mingo.ts
+++ src/rx-query-mingo.ts
@@ -1,27 +1,30 @@
+/**
+ * TODO
+ * FIX @link https://github.com/kofrasa/mingo/issues/308
+ */
 import { useOperators, OperatorType } from 'mingo/core';
 import { Query } from 'mingo/query';
 import type { MangoQuerySelector } from './types';
-import { $sort, $project } from 'mingo/operators/pipeline';
-import {
-    $and,
-    $eq,
-    $elemMatch,
-    $exists,
-    $gt,
-    $gte,
-    $in,
-    $lt,
-    $lte,
-    $ne,
-    $nin,
-    $mod,
-    $nor,
-    $not,
-    $or,
-    $regex,
-    $size,
-    $type,
-} from 'mingo/operators/query';
+import { $project } from 'mingo/operators/pipeline/project';
+import { $sort } from 'mingo/operators/pipeline/sort';
+import { $and } from 'mingo/operators/query/logical/and';
+import { $not } from 'mingo/operators/query/logical/not';
+import { $or } from 'mingo/operators/query/logical/or';
+import { $nor } from 'mingo/operators/query/logical/nor';
+import { $eq } from 'mingo/operators/query/comparison/eq';
+import { $ne } from 'mingo/operators/query/comparison/ne';
+import { $gt } from 'mingo/operators/query/comparison/gt';
+import { $gte } from 'mingo/operators/query/comparison/gte';
+import { $lt } from 'mingo/operators/query/comparison/lt';
+import { $lte } from 'mingo/operators/query/comparison/lte';
+import { $regex } from 'mingo/operators/query/evaluation/regex';
+import { $mod } from 'mingo/operators/query/evaluation/mod';
+import { $elemMatch } from 'mingo/operators/query/array/elemMatch';
+import { $exists } from 'mingo/operators/query/element/exists';
+import { $nin } from 'mingo/operators/query/comparison/nin';
+import { $in } from 'mingo/operators/query/comparison/in';
+import { $size } from 'mingo/operators/query/array/size';
+import { $type } from 'mingo/operators/expression/type/type';
 
 let mingoInitDone = false;
 
