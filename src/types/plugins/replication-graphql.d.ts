77338fd07ced8834db615a0c9b1ed8a58f2c940b
Johnny van den Berg <jvdberg@moreapp.com>
2023-03-09 13:54:30 +0100

    ADD 'includeHttpHeadersWs' property for GraphQL replication

--- src/types/plugins/replication-graphql.d.ts
+++ src/types/plugins/replication-graphql.d.ts
@@ -33,6 +33,7 @@ ReplicationPullOptions<RxDocType, CheckpointType>,
     streamQueryBuilder?: RxGraphQLReplicationPullStreamQueryBuilder;
     dataPath?: string;
     responseModifier?: RxGraphQLPullResponseModifier<RxDocType, CheckpointType>;
+    includeHttpHeadersWs?: boolean;
 };
 
 export type RxGraphQLPullResponseModifier<RxDocType, CheckpointType> = (
