5f0b09fbbd902a2d867bfc6acf9f5cad10b1bfc2
Daniel Meyer <8926560+pubkey@users.noreply.github.com>
2023-03-14 00:51:55 +0100

    ADD test (#4554)
    * ADD test
    
    * ADD test

--- test/unit/rx-storage-implementations.test.ts
+++ test/unit/rx-storage-implementations.test.ts
@@ -949,6 +949,49 @@ config.parallel('rx-storage-implementations.test.ts (implementation: ' + config.
 
                 storageInstance.close();
             });
+            it('must be able create multiple storage instances on the same database and write documents', async () => {
+                const collectionsAmount = 3;
+                const docsAmount = 3;
+                const databaseName = randomCouchString(10);
+                const databaseInstanceToken = randomCouchString(10);
+
+                const storageInstances = await Promise.all(
+                    new Array(collectionsAmount)
+                        .fill(0)
+                        .map(async () => {
+                            const storageInstance = await config.storage.getStorage().createStorageInstance<TestDocType>({
+                                databaseInstanceToken,
+                                databaseName,
+                                collectionName: randomCouchString(12),
+                                schema: getPseudoSchemaForVersion<TestDocType>(0, 'key'),
+                                options: {},
+                                multiInstance: false,
+                                devMode: true
+                            });
+                            await Promise.all(
+                                new Array(docsAmount)
+                                    .fill(0)
+                                    .map(async (_v, docId) => {
+                                        const writeData: RxDocumentWriteData<TestDocType> = {
+                                            key: docId + '',
+                                            value: randomCouchString(5),
+                                            _rev: EXAMPLE_REVISION_1,
+                                            _deleted: false,
+                                            _meta: {
+                                                lwt: now()
+                                            },
+                                            _attachments: {}
+                                        };
+                                        await storageInstance.bulkWrite([{ document: writeData }], testContext);
+                                    })
+                            );
+                            return storageInstance;
+                        })
+                );
+                await Promise.all(
+                    storageInstances.map(i => i.close())
+                );
+            });
         });
         describe('.prepareQuery()', () => {
             it('must not crash', () => {
