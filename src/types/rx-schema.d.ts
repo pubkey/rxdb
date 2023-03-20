766eccda63057b72b7d139f9ffda471ef242786e
Johannes Millan <johannesjo@users.noreply.github.com>
2023-03-20 17:26:53 +0100

    fix: typescript schema to allow nullable fields
    Otherwise any schema with a type like:
    ```
       myfield: {
          type: ["string", "null"],
        },
    ```
    will produce an error.

--- src/types/rx-schema.d.ts
+++ src/types/rx-schema.d.ts
@@ -5,7 +5,7 @@ import { StringKeys } from './util';
 /**
  * @link https://github.com/types/lib-json-schema/blob/master/v4/index.d.ts
  */
-export type JsonSchemaTypes = 'array' | 'boolean' | 'integer' | 'number' | 'null' | 'object' | 'string' | (string & {});
+export type JsonSchemaTypes =  'array' | 'boolean' | 'integer' | 'number' | 'null' | 'object' | 'string' | (string & {}) | ['null', 'array'] | ['null', 'boolean'] | ['null', 'integer'] | ['null', 'number'] | ['null', 'object'] | ['null', 'string'];
 
 export type CompositePrimaryKey<RxDocType> = {
     /**
