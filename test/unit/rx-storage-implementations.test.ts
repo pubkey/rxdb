4e7df7a17ad70c80701ffb0c4615232e2f3ca611
Daniel Meyer <8926560+pubkey@users.noreply.github.com>
2023-05-19 15:12:14 +0200

    Bugfix/umlauts not working (#4747)
    * ADD test for umlauts
    
    * FIX test
    
    * ADD more test for umlauts
    
    * FIX test
    
    * FIX test

--- test/unit/rx-storage-implementations.test.ts
+++ test/unit/rx-storage-implementations.test.ts
@@ -1035,6 +1035,27 @@ config.parallel('rx-storage-implementations.test.ts (implementation: ' + config.
                     'value' + umlauts
                 );
 
+
+                // store another doc
+                const docData2: RxDocumentData<TestDocType> = {
+                    key: 'foobar2' + umlauts,
+                    value: 'value2' + umlauts,
+                    _deleted: false,
+                    _meta: {
+                        lwt: now()
+                    },
+                    _rev: EXAMPLE_REVISION_1,
+                    _attachments: {}
+                };
+                await storageInstance.bulkWrite(
+                    [{
+                        document: clone(docData2)
+                    }],
+                    testContext
+                );
+                const getDocFromDb2 = await storageInstance.findDocumentsById([docData2.key], false);
+                getFromObjectOrThrow(getDocFromDb2, docData2.key);
+
                 storageInstance.close();
             });
         });
