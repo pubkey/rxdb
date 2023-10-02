3041f4f8b12bf285f03a3a4feca51b2f528dec8a
pubkey <8926560+pubkey@users.noreply.github.com>
2023-10-02 12:44:25 +0200

    FIX close broadcast channel

--- src/plugins/migration/rx-migration-state.ts
+++ src/plugins/migration/rx-migration-state.ts
@@ -146,6 +146,7 @@ export class RxMigrationState {
             ].join('|'));
             const leaderElector = createLeaderElection(broadcastChannel);
             await leaderElector.awaitLeadership();
+            await broadcastChannel.close();
         }
 
         /**
