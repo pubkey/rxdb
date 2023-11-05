dcd3b90caa1e43411d0d5eccbe785719ea001d4a
pubkey <8926560+pubkey@users.noreply.github.com>
2023-11-05 13:51:45 +0100

    UPDATe pricing

--- docs-src/premium.ts
+++ docs-src/premium.ts
@@ -11,112 +11,115 @@ const FORM_VALUE_DOCUMENT_ID = 'premium-price-form-value';
 type FormValueDocData = {
     teamSize: number;
     homeCountry: string;
-    companyAge: CompanyAge;
+    companySize: number;
 };
 
-window.onload = async function () {
-    const $homeCountry = ensureNotFalsy(document.getElementById('home-country'));
-    const $priceCalculatorForm: HTMLFormElement = ensureNotFalsy(document.getElementById('price-calculator-form')) as any;
-    const $priceCalculatorSubmit = ensureNotFalsy(document.getElementById('price-calculator-submit'));
+if (typeof window !== 'undefined') {
+    window.onload = async function () {
+        const $homeCountry = ensureNotFalsy(document.getElementById('home-country'));
+        const $priceCalculatorForm: HTMLFormElement = ensureNotFalsy(document.getElementById('price-calculator-form')) as any;
+        const $priceCalculatorSubmit = ensureNotFalsy(document.getElementById('price-calculator-submit'));
 
-    const $priceCalculatorResult = ensureNotFalsy(document.getElementById('price-calculator-result'));
-    const $priceCalculatorResultPerMonth = ensureNotFalsy(document.getElementById('total-per-developer-per-month'));
-    const $priceCalculatorResultPerYear = ensureNotFalsy(document.getElementById('total-per-year'));
+        const $priceCalculatorResult = ensureNotFalsy(document.getElementById('price-calculator-result'));
+        const $priceCalculatorResultPerMonth = ensureNotFalsy(document.getElementById('total-per-developer-per-month'));
+        const $priceCalculatorResultPerYear = ensureNotFalsy(document.getElementById('total-per-year'));
 
 
-    AVERAGE_FRONT_END_DEVELOPER_SALARY_BY_COUNTRY
-        .sort((a, b) => a.code >= b.code ? 1 : -1)
-        .forEach(country => {
-            const option = document.createElement('option');
+        AVERAGE_FRONT_END_DEVELOPER_SALARY_BY_COUNTRY
+            .sort((a, b) => a.code >= b.code ? 1 : -1)
+            .forEach(country => {
+                const option = document.createElement('option');
+
+                /**
+                 * Do not use the country.code as option.value
+                 * because it would create a broken datalist on
+                 * iOS safari.
+                 */
+                option.value = country.name;
+
+                option.innerHTML = country.name;
+                $homeCountry.appendChild(option);
+            });
+
+
+        const database = await dbPromise;
+        const formValueDoc = await database.getLocal<FormValueDocData>(FORM_VALUE_DOCUMENT_ID);
+        // if (formValueDoc) {
+        //     setToInput('developer-count', formValueDoc._data.data.teamSize);
+        //     setToInput('home-country', formValueDoc._data.data.homeCountry);
+        //     setToInput('company-size', formValueDoc._data.data.companySize);
+        // }
+
+        $priceCalculatorSubmit.onclick = async () => {
+            (window as any).trigger('calculate_premium_price', 3);
+
+            const isValid = ($priceCalculatorForm as any).reportValidity();
+            if (!isValid) {
+                console.log('form not valid');
+                return;
+            }
+            const formDataPlain = new FormData($priceCalculatorForm);
+            const formData = Object.fromEntries(formDataPlain.entries());
+
+            console.log('formData:');
+            console.dir(formData);
+
+
+            const homeCountry = AVERAGE_FRONT_END_DEVELOPER_SALARY_BY_COUNTRY
+                .find(o => o.name.toLowerCase() === (formData['home-country'] as string).toLowerCase());
+            if (!homeCountry) {
+                return;
+            }
 
             /**
-             * Do not use the country.code as option.value
-             * because it would create a broken datalist on
-             * iOS safari.
+             * Save the input
+             * so we have to not re-insert manually on page reload.
              */
-            option.value = country.name;
+            await database.upsertLocal<FormValueDocData>(FORM_VALUE_DOCUMENT_ID, {
+                companySize: formData['company-size'] as any,
+                teamSize: formData['developer-count'] as any,
+                homeCountry: homeCountry.name
+            });
 
-            option.innerHTML = country.name;
-            $homeCountry.appendChild(option);
-        });
+            const packages: PackageName[] = Object.keys(formData)
+                .filter(k => k.startsWith('package-'))
+                .map(k => lastOfArray(k.split('-')) as any);
 
+            const priceResult = calculatePrice({
+                companySize: formData['company-size'] as any,
+                teamSize: formData['developer-count'] as any,
+                homeCountryCode: homeCountry.code,
+                packages
+            });
+            console.log('priceResult:');
+            console.log(JSON.stringify(priceResult, null, 4));
 
-    const database = await dbPromise;
-    const formValueDoc = await database.getLocal<FormValueDocData>(FORM_VALUE_DOCUMENT_ID);
-    if (formValueDoc) {
-        setToInput('developer-count', formValueDoc._data.data.teamSize);
-        setToInput('home-country', formValueDoc._data.data.homeCountry);
-        setToInput('company-age', formValueDoc._data.data.companyAge);
-    }
-
-    $priceCalculatorSubmit.onclick = async () => {
-        (window as any).trigger('calculate_premium_price', 3);
-
-        const isValid = ($priceCalculatorForm as any).reportValidity();
-        if (!isValid) {
-            console.log('form not valid');
-            return;
-        }
-        const formDataPlain = new FormData($priceCalculatorForm);
-        const formData = Object.fromEntries(formDataPlain.entries());
-
-        console.log('formData:');
-        console.dir(formData);
-
-
-        const homeCountry = AVERAGE_FRONT_END_DEVELOPER_SALARY_BY_COUNTRY
-            .find(o => o.name.toLowerCase() === (formData['home-country'] as string).toLowerCase());
-        if (!homeCountry) {
-            return;
-        }
-
-        /**
-         * Save the input
-         * so we have to not re-insert manually on page reload.
-         */
-        await database.upsertLocal<FormValueDocData>(FORM_VALUE_DOCUMENT_ID, {
-            companyAge: formData['company-age'] as any,
-            teamSize: formData['developer-count'] as any,
-            homeCountry: homeCountry.name
-        });
-
-        const packages: string[] = Object.keys(formData)
-            .filter(k => k.startsWith('package-'))
-            .map(k => lastOfArray(k.split('-')) as any);
-
-        const priceResult = calculatePrice({
-            companyAge: formData['company-age'] as any,
-            teamSize: formData['developer-count'] as any,
-            homeCountryCode: homeCountry.code,
-            packages
-        });
-        console.log('priceResult:');
-        console.log(JSON.stringify(priceResult, null, 4));
-
-        const getConverterUrl = (price: number) => {
-            return 'https://www.xe.com/en/currencyconverter/convert/?Amount=' + price + '&From=EUR&To=USD';
+            const getConverterUrl = (price: number) => {
+                return 'https://www.xe.com/en/currencyconverter/convert/?Amount=' + price + '&From=EUR&To=USD';
+            };
+            const setPrice = (element: typeof $priceCalculatorResultPerMonth, price: number) => {
+                element.innerHTML = price + ' &euro; (EUR)';
+                (element as any).href = getConverterUrl(price);
+            };
+            setPrice($priceCalculatorResultPerMonth, priceResult.perDeveloperPerMonth);
+            setPrice($priceCalculatorResultPerYear, priceResult.totalPerYear);
+            $priceCalculatorResult.style.display = 'block';
         };
-        const setPrice = (element: typeof $priceCalculatorResultPerMonth, price: number) => {
-            element.innerHTML = price + ' &euro; (EUR)';
-            (element as any).href = getConverterUrl(price);
-        };
-        setPrice($priceCalculatorResultPerMonth, priceResult.perDeveloperPerMonth);
-        setPrice($priceCalculatorResultPerYear, priceResult.totalPerYear);
-        $priceCalculatorResult.style.display = 'block';
     };
-};
+}
 
 
 
 /**
  * Prices are in percent of average salary.
  */
-export const PACKAGE_PRICE: { [k: string]: number; } = {
-    browser: 0.49,
-    native: 0.65,
-    performance: 0.30,
-    // source-code access has no base price but only adds x% to the total.
-    sourcecode: 0
+export const PACKAGE_PRICE: { [k in PackageName]: number; } = {
+    browser: 0.30,
+    native: 0.30,
+    performance: 0.25,
+    // source-code access and others have no base price but only adds x% to the total.
+    sourcecode: 0,
+    perpetual: 0
 };
 
 /**
@@ -124,7 +127,7 @@ export const PACKAGE_PRICE: { [k: string]: number; } = {
  */
 export const INFLATION_RATE = 0.05;
 
-type CompanyAge = 'more-than-3' | 'less-than-3';
+export type PackageName = 'perpetual' | 'sourcecode' | 'browser' | 'native' | 'performance';
 
 /**
  * All prices are in Euro €
@@ -132,12 +135,12 @@ type CompanyAge = 'more-than-3' | 'less-than-3';
 export function calculatePrice(input: {
     teamSize: number;
     homeCountryCode: string;
-    companyAge: CompanyAge;
-    packages: string[];
+    companySize: number;
+    licensePeriod: number;
+    packages: PackageName[];
 }) {
 
     const baseFee = 400;
-    const baseFeePerDeveloper = 90;
     const country = ensureNotFalsy(AVERAGE_FRONT_END_DEVELOPER_SALARY_BY_COUNTRY.find(c => c.code === input.homeCountryCode));
     const developerSalary = country.salary;
 
@@ -148,38 +151,47 @@ export function calculatePrice(input: {
     });
     console.log('aimInPercent: ' + aimInPercent);
 
-    let totalPerYear = baseFee +
-        (baseFeePerDeveloper * input.teamSize) +
-        (developerSalary * (aimInPercent / 100) * Math.pow(input.teamSize, 0.6));
 
-    /**
-     * Discount for 'young' companies.
-     */
-    if (input.companyAge === 'less-than-3') {
-        totalPerYear = totalPerYear * 0.80;
-    }
+    let totalPrice = baseFee + ((developerSalary * 1.0) * (aimInPercent / 100));
 
     /**
      * Discount if more then one package
      */
-    if (input.packages.length > 1) {
-        totalPerYear = totalPerYear * 0.90;
+    if (input.packages.length === 2) {
+        totalPrice = totalPrice * 0.90;
+    }
+    if (input.packages.length > 2) {
+        totalPrice = totalPrice * 0.85;
+    }
+
+    /**
+     * Increase price for bigger companies
+     * @link https://www.geogebra.org/graphing
+     */
+    if (input.companySize > 1) {
+        const companySizeIncrease = 1 + ((Math.pow(input.companySize - 1, 0.60) / 100) * 1.5);
+        console.log('input.companySize ' + input.companySize + ' - ' + companySizeIncrease);
+        totalPrice = totalPrice * companySizeIncrease;
     }
 
     /**
      * Add price for source-code read access
      */
     if (input.packages.includes('sourcecode')) {
-        totalPerYear = totalPerYear * 1.33;
+        totalPrice = totalPrice * 1.75;
 
         /**
          * Providing source code access is complex,
          * so there is a minimum price.
          */
-        const minPriceForSourceCodeAccess = 2520;
-        if (totalPerYear < minPriceForSourceCodeAccess) {
-            totalPerYear = minPriceForSourceCodeAccess;
+        const minPriceForSourceCodeAccess = 1520;
+        if (totalPrice < minPriceForSourceCodeAccess) {
+            totalPrice = minPriceForSourceCodeAccess;
         }
+    }
+
+    if (input.packages.includes('perpetual')) {
+        totalPrice = totalPrice * 1.45;
     }
 
     /**
@@ -190,12 +202,14 @@ export function calculatePrice(input: {
     const yearsDiff = currentYear - baseYear;
     const inflationMultiplier = Math.pow((1 + INFLATION_RATE), yearsDiff);
     console.log('inflationMultiplier: ' + inflationMultiplier);
-    totalPerYear = totalPerYear * inflationMultiplier;
+    totalPrice = totalPrice * inflationMultiplier;
 
-    totalPerYear = Math.ceil(totalPerYear);
+    totalPrice = Math.ceil(totalPrice) * input.licensePeriod;
+
+
     return {
-        totalPerYear,
-        perDeveloperPerMonth: Math.floor(totalPerYear / 12 / input.teamSize)
+        totalPrice,
+        perDeveloperPerMonth: Math.floor(totalPrice / (12 * input.licensePeriod) / input.teamSize)
     };
 }
 
