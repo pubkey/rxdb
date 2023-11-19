a44885fe1e54b9924e984cdf379b68dcbd79c4b4
pubkey <8926560+pubkey@users.noreply.github.com>
2023-11-19 03:15:59 +0100

    CHORE docusaurus

---
+++ rxdb/docs/foobar.md
@@ -0,0 +1,9 @@
+---
+title: foobar
+slug: foobar.html
+description: my bfoobar description
+---
+
+# Markdown page example
+
+You don't need React to write simple standalone pages.
---
+++ rxdb/docusaurus.config.js
@@ -0,0 +1,159 @@
+// @ts-check
+// `@type` JSDoc annotations allow editor autocompletion and type checking
+// (when paired with `@ts-check`).
+// There are various equivalent ways to declare your Docusaurus config.
+// See: https://docusaurus.io/docs/api/docusaurus-config
+
+import { themes as prismThemes } from 'prism-react-renderer';
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
+  title: 'RxDB - JavaScript Database',
+  tagline: 'Realtime JavaScript Database',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://rxdb.info',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'pubkey', // Usually your GitHub org/user name.
+  projectName: 'rxdb', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
+        docs: {
+          sidebarPath: './sidebars.js',
+          editUrl: 'https://github.com/pubkey/rxdb/tree/main/docs/',
+          routeBasePath: '',
+          path: './docs',
+        },
+        // blog: {
+        //   showReadingTime: true,
+        //   editUrl:
+        //     'https://github.com/pubkey/rxdb/tree/main/docs/',
+        // },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      }),
+    ],
+  ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+
+      // Replace with your project's social card
+      image: 'img/rxdb_social_card.png',
+      colorMode: {
+        defaultMode: 'dark',
+        disableSwitch: true,
+        respectPrefersColorScheme: false,
+      },
+      navbar: {
+        title: 'RxDB',
+        logo: {
+          alt: 'RxDB Logo',
+          src: 'files/logo/logo.svg',
+        },
+        items: [
+          {
+            type: 'docSidebar',
+            sidebarId: 'tutorialSidebar',
+            position: 'left',
+            label: 'Tutorial',
+          },
+          { to: '/blog', label: 'Blog', position: 'left' },
+          {
+            href: '/premium.html',
+            label: 'Pricing',
+            position: 'right',
+          },
+          {
+            href: '/quickstart.html',
+            label: 'Docs',
+            position: 'right',
+          },
+          {
+            href: 'https://github.com/pubkey/rxdb',
+            label: 'Code',
+            position: 'right',
+          },
+          {
+            href: '/chat.html',
+            label: 'Chat',
+            position: 'right',
+          },
+        ],
+      },
+      footer: {
+        style: 'dark',
+        links: [
+          {
+            title: 'Docs',
+            items: [
+              {
+                label: 'Tutorial',
+                to: '/docs/intro',
+              },
+            ],
+          },
+          {
+            title: 'Community',
+            items: [
+              {
+                label: 'Stack Overflow',
+                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+              },
+              {
+                label: 'Discord',
+                href: 'https://discordapp.com/invite/docusaurus',
+              },
+              {
+                label: 'Twitter',
+                href: 'https://twitter.com/docusaurus',
+              },
+            ],
+          },
+          {
+            title: 'More',
+            items: [
+              {
+                label: 'Blog',
+                to: '/blog',
+              },
+              {
+                label: 'GitHub',
+                href: 'https://github.com/facebook/docusaurus',
+              },
+            ],
+          },
+        ],
+        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: prismThemes.github,
+        darkTheme: prismThemes.dracula,
+      },
+    }),
+};
+
+export default config;
