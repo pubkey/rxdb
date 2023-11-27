7486ce9607c477c4e56923b5712021f68ab2f81f
Daniel Meyer <8926560+pubkey@users.noreply.github.com>
2023-11-27 13:50:21 +0100

    Bugfix/no structuredClone on react native (#5321)
    * FIX randomly failing test
    
    * FIX `structuredClone` not available in ReactNative

--- src/rx-storage-helper.ts
+++ src/rx-storage-helper.ts
@@ -580,14 +580,22 @@ export function getWrappedStorageInstance<
              * Ensure it can be structured cloned
              */
             try {
-                structuredClone(writeRow);
+                /**
+                 * Notice that structuredClone() is not available
+                 * in ReactNative, so we test for JSON.stringify() instead
+                 * @link https://github.com/pubkey/rxdb/issues/5046#issuecomment-1827374498
+                 */
+                if (typeof structuredClone === 'function') {
+                    structuredClone(writeRow);
+                } else {
+                    JSON.parse(JSON.stringify(writeRow));
+                }
             } catch (err) {
                 throw newRxError('DOC24', {
                     collection: storageInstance.collectionName,
                     document: writeRow.document
                 });
             }
-
 
             /**
              * Ensure that the new revision is higher
