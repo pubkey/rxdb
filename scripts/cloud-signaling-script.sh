e4ae845af1c08485e914d85aaebef0e379f7f5ea
pubkey <8926560+pubkey@users.noreply.github.com>
2023-12-02 21:21:56 +0100

    FIX memory limit

--- scripts/cloud-signaling-script.sh
+++ scripts/cloud-signaling-script.sh
@@ -3,4 +3,4 @@ set -e
 
 cd /rxdb
 npm install forever -g
-nohup forever start -c "node --max_old_space_size=512" ./scripts/start-cloud-signaling-server.mjs &
+nohup forever start -c "node --max_old_space_size=2024" ./scripts/start-cloud-signaling-server.mjs &
