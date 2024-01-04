d0063592ed5f30b7c6aad98a498f82be51c37403
anhoev <anhoev@yahoo.de>
2024-01-04 23:49:03 +0700

    feature : initialCheckpoint for couchdb

--- src/plugins/replication-couchdb/index.ts
+++ src/plugins/replication-couchdb/index.ts
@@ -136,7 +136,8 @@ export function replicateCouchDB<RxDocType>(
             },
             batchSize: ensureNotFalsy(options.pull).batchSize,
             modifier: ensureNotFalsy(options.pull).modifier,
-            stream$: pullStream$.asObservable()
+            stream$: pullStream$.asObservable(),
+            initialCheckpoint: options.pull.initialCheckpoint
         };
     }
 
@@ -251,7 +252,8 @@ export function replicateCouchDB<RxDocType>(
                 return conflicts;
             },
             batchSize: options.push.batchSize,
-            modifier: options.push.modifier
+            modifier: options.push.modifier,
+            initialCheckpoint: options.push.initialCheckpoint
         };
     }
 
