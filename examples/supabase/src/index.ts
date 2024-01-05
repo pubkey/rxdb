770ad3e2525563a5e047a3cf99df00357a2feea6
Marc MacLeod <847542+marbemac@users.noreply.github.com>
2024-01-05 10:54:51 -0500

    fix(deps): bump dexie to fix indexedDB.open error
    and resolve flakey supabase ci tests
    
    Signed-off-by: Marc MacLeod <847542+marbemac@users.noreply.github.com>

--- examples/supabase/src/index.ts
+++ examples/supabase/src/index.ts
@@ -1,14 +1,29 @@
-
 import '../public/style.css';
 
 import { startReplication } from './replication';
 import { createDatabase } from './database';
 import { addClickHandlers } from './handlers';
 
-const heroesList = document.querySelector('#heroes-list');
+window.addEventListener('unhandledrejection', (e) => {
+    console.error(
+        `got unhandled error`,
+        e.reason.message,
+        JSON.stringify({
+            // @ts-expect-error ignore
+            frame: e.currentTarget?.ifrm?.id,
+        }),
+        e
+    );
+});
 
 async function run() {
-    console.log('run()');
+    console.log('run()', { location: window.location.pathname });
+
+    const heroesList = document.querySelector('#heroes-list');
+    if (!heroesList) {
+        // if no heroes list we're on some other page like multitab, skip
+        return;
+    }
 
     heroesList.innerHTML = 'Create database..';
     const database = await createDatabase();
@@ -16,13 +31,13 @@ async function run() {
     heroesList.innerHTML = 'Subscribe to query..';
     database.heroes
         .find({
-            sort: [{ name: 'asc' }]
-        }).$
-        .subscribe(heroes => {
+            sort: [{ name: 'asc' }],
+        })
+        .$.subscribe((heroes) => {
             console.log('emitted heroes:');
-            console.dir(heroes.map(d => d.toJSON(true)));
+            console.dir(heroes.map((d) => d.toJSON(true)));
             let html = '';
-            heroes.forEach(hero => {
+            heroes.forEach((hero) => {
                 html += `
                     <li class="hero-item">
                         <div class="color-box" style="background:${hero.color}"></div>
@@ -33,7 +48,6 @@ async function run() {
             });
             heroesList.innerHTML = html;
         });
-
 
     startReplication(database);
     addClickHandlers(database);
