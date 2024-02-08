c458a7ee0a6a8afef84f9841bbff85e1d9cd7af2
pubkey <8926560+pubkey@users.noreply.github.com>
2024-02-08 14:05:30 +0100

    FIX round price

--- docs-src/src/components/price-calculator.ts
+++ docs-src/src/components/price-calculator.ts
@@ -140,6 +140,18 @@ export function calculatePrice(input: PriceCalculationInput) {
     }
 
     totalPrice = Math.ceil(totalPrice);
+
+
+    /**
+     * Stripe does not allow to create abos on flexibles prices
+     * so we just round the value so we can use precreated stripe products.
+     */
+    if (totalPrice > 1200) {
+        totalPrice = Math.floor(totalPrice / 100) * 100;
+    } else {
+        totalPrice = Math.floor(totalPrice / 50) * 50;
+    }
+
     return {
         totalPrice
     };
