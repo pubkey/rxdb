e78084bd01a1aa23cdab99bd7a94830dd4075990
pubkey <8926560+pubkey@users.noreply.github.com>
2024-03-06 15:28:25 +0100

    FIX typo

--- src/types/rx-document.d.ts
+++ src/types/rx-document.d.ts
@@ -27,7 +27,7 @@ export type RxDocument<RxDocumentType = {}, OrmMethods = {}, Reactivity = unknow
 /**
  * Extend the base properties by the property$ fields
  * so it knows that RxDocument.age also has RxDocument.age$ which is
- * and observable.
+ * an observable.
  * TODO how to do this for the nested fields?
  */
 type ExtendObservables<RxDocumentType> = {
