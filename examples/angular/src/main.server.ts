3887a99ba6e4d51dbf04d6ce7c83d4624b2355cb
pubkey <8926560+pubkey@users.noreply.github.com>
2024-03-26 22:13:20 +0100

    DISABLE angular ssr

--- examples/angular/src/main.server.ts
+++ examples/angular/src/main.server.ts
@@ -1,6 +1,7 @@
 import { enableProdMode } from '@angular/core';
-
+import { bootstrapApplication } from '@angular/platform-browser';
 import { environment } from './environments/environment';
+import { AppComponent } from './app/app.component';
 
 if (environment.production) {
   enableProdMode();
@@ -8,3 +9,4 @@ if (environment.production) {
 
 export { AppServerModule } from './app/app.server.module';
 export { renderModule } from '@angular/platform-server';
+export const bootstrap = () => bootstrapApplication(AppComponent);
