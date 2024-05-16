8fcb42657cf8e5b56f4d677a4afaae163391288b
pubkey <8926560+pubkey@users.noreply.github.com>
2024-05-16 11:37:54 +0200

    ADD pipedrive chat

--- docs-src/src/theme/Root.tsx
+++ docs-src/src/theme/Root.tsx
@@ -8,7 +8,9 @@ export default function Root({ children }) {
         if (!isBrowser) {
             return;
         }
-        addCommunityChatButton();
+
+        // addCommunityChatButton();
+
         addCallToActionButton();
 
     });
