39d2d64feff122bb34226d830e4f3b7b2cf5a5f8
東奈比 <zhouzixian@qq.com>
2024-05-27 16:47:53 +0800

    fix: ignore type check

--- scripts/fix-types.mjs
+++ scripts/fix-types.mjs
@@ -5,6 +5,8 @@ async function main () {
     try {
         let content = await fs.readFile(file, { encoding: 'utf-8' })
         content = content.replaceAll('.ts', '.d.ts')
+        content = `// @ts-nocheck
+        ${content}`
         await fs.writeFile(file, content)
     } catch (err) {
         console.error(`Fix types error：${err.message}`)
