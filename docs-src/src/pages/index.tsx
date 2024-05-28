3c40edf3b965106e1cdd64555ccf10f9d87ea56f
pubkey <8926560+pubkey@users.noreply.github.com>
2024-05-28 12:26:00 +0200

    CHORE

--- docs-src/src/pages/index.tsx
+++ docs-src/src/pages/index.tsx
@@ -26,7 +26,7 @@ import { trigger } from '../components/trigger-event';
 
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
-import { ReviewsBlock } from '../components/review-block';
+import { REVIEW_ITEMS, ReviewsBlock } from '../components/review-block';
 import { BrowserWindow } from '../components/browser-window';
 import { TagCloud } from 'react-tagcloud';
 
@@ -362,8 +362,6 @@ export default function Home() {
                     The local <b className="underline">Database</b> for{' '}
                     <b className="underline">JavaScript</b> Applications
                   </h1>
-                  <br />
-                  <br />
                   {/* <ul className="checked">
                     <li>
                       <b>Offline Support</b>:
@@ -395,6 +393,42 @@ export default function Home() {
                   <div className="text">
                     Store data locally to build high performance realtime applications that sync data with the backend and even work when offline.
                   </div>
+
+                  <br />
+                  <br />
+
+                  <a
+                    className="button"
+                    href="/quickstart.html"
+                    onClick={() => trigger('start_now', 0.4)}
+                  >
+                    Get Started »
+                  </a>
+                  <a
+                    href="/premium#price-calculator-block"
+                    onClick={() => trigger('request_premium_main_page', 3)}
+                    className='buy-premium-hero'
+                  >
+                    Buy Premium
+                  </a>
+                  {/* <a
+                    className="button light"
+                    href="/code"
+                    target="_blank"
+                  >
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      viewBox="0 0 16 16"
+                      className="star-icon"
+                      aria-hidden="true"
+                      fill="currentColor"
+                      style={{ width: 14, marginRight: 8, marginLeft: -6, float: 'left', marginTop: 2 }}
+                    >
+                      <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z" />
+                    </svg>
+                    Star (20,172)
+                  </a> */}
+
                   <div className="clear" />
                   <br />
                 </div>
@@ -426,42 +460,10 @@ export default function Home() {
                 <div className='clear'></div>
               </div>
             </div>
-            <div className='content'>
-              <a
-                className="button"
-                href="/quickstart.html"
-                onClick={() => trigger('start_now', 0.4)}
-              >
-                Get Started
-              </a>
-              {/* <a
-                className="button"
-                href="/premium#price-calculator-block"
-                onClick={() => trigger('request_premium_main_page', 3)}
-              >
-                Get Now
-              </a> */}
-              <a
-                className="button light"
-                href="/code"
-                target="_blank"
-              >
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  className="star-icon"
-                  aria-hidden="true"
-                  fill="currentColor"
-                  style={{ width: 14, marginRight: 8, marginLeft: -6, float: 'left', marginTop: 2 }}
-                >
-                  <path d="M8 0c4.42 0 8 3.58 8 8a8.013 8.013 0 0 1-5.45 7.59c-.4.08-.55-.17-.55-.38 0-.27.01-1.13.01-2.2 0-.75-.25-1.23-.54-1.48 1.78-.2 3.65-.88 3.65-3.95 0-.88-.31-1.59-.82-2.15.08-.2.36-1.02-.08-2.12 0 0-.67-.22-2.2.82-.64-.18-1.32-.27-2-.27-.68 0-1.36.09-2 .27-1.53-1.03-2.2-.82-2.2-.82-.44 1.1-.16 1.92-.08 2.12-.51.56-.82 1.28-.82 2.15 0 3.06 1.86 3.75 3.64 3.95-.23.2-.44.55-.51 1.07-.46.21-1.61.55-2.33-.66-.15-.24-.6-.83-1.23-.82-.67.01-.27.38.01.53.34.19.73.9.82 1.13.16.45.68 1.31 2.69.94 0 .67.01 1.3.01 1.49 0 .21-.15.45-.55.38A7.995 7.995 0 0 1 0 8c0-4.42 3.58-8 8-8Z" />
-                </svg>
-                Star (20,172)
-              </a>
-              <div className="clear"></div>
-              <br />
-              <br />
-            </div>
+            <br />
+            <br />
+            <br />
+            <br />
           </div >
 
           <a
@@ -501,7 +503,7 @@ export default function Home() {
                 applications fast and reliable.{' '}
                 {/* It does not matter if the data was changed by{' '}                <b>a user event</b>, <b>another browser tab</b> or by the<b> replication</b> */}
                 Whenever your data changes, your UI reflects the new state.{' '}
-                RxDB supports <b>RxJS</b> and other <a href="/reactivity.html" target="_blank">any reactiveness libraries</a> like signals, hooks or vue.js-refs.
+                RxDB supports <b>RxJS</b> and <a href="/reactivity.html" target="_blank">any reactiveness libraries</a> like signals, hooks or vue.js-refs.
               </p>
               <div className="inner">
                 {/*
