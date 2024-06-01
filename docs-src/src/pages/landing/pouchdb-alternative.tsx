c8a05f20c7015b7087a5054ee8a2f17ff93a629e
pubkey <8926560+pubkey@users.noreply.github.com>
2024-06-01 14:31:13 +0200

    ADD pagese

---
+++ docs-src/src/pages/landing/pouchdb-alternative.tsx
@@ -0,0 +1,10 @@
+import Home from '..';
+
+export default function Page() {
+    return Home({
+        metaTitle: 'The NoSQL alternative for PouchDB',
+        title: <>The <b className="underline">modern</b> alternative for{' '}
+            <b className="underline">PouchDB</b></>,
+        text: <>Store data inside the Browser to build high performance realtime applications that sync data from the backend and even work when offline.</>
+    });
+}
