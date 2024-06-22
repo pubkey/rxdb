8c3026e716b4d849b1634aff87b8b7a2241571cd
Chad Wood <mr.chadmwood@gmail.com>
2024-06-22 15:17:22 -0700

    Update unit.test.ts

--- test/unit.test.ts
+++ test/unit.test.ts
@@ -19,6 +19,7 @@ import './unit/internal-indexes.test.ts';
  * your relevant tests run first.
  * Do not commit this file if you modified the order.
  */
+import './unit/onRemove-missing.test.js'
 import './unit/rx-storage-implementations.test.ts';
 import './unit/rx-storage-query-correctness.test.ts';
 import './unit/rx-storage-helper.test.ts';
