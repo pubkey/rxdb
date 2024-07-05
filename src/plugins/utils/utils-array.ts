be83a0f67a1d0d7aa9f0ac9510f8729dbfb8e288
pubkey <8926560+pubkey@users.noreply.github.com>
2024-07-05 16:14:21 +0200

    ADD hint

--- src/plugins/utils/utils-array.ts
+++ src/plugins/utils/utils-array.ts
@@ -144,7 +144,7 @@ export function maxOfNumbers(arr: number[]): number {
  * @link https://www.measurethat.net/Benchmarks/Show/4223/0/array-concat-vs-spread-operator-vs-push#latest_results_block
  * 
  * TODO it turns out that in mid 2024 v8 has optimzied Array.concat()
- * so it might be faster to just use concate again:
+ * so it might be faster to just use concat() again:
  * @link https://jsperf.app/qiqawa/10
  */
 export function appendToArray<T>(ar: T[], add: T[] | readonly T[]): void {
