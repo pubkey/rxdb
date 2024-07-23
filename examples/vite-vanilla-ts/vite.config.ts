e2531c16487692f3b6663bb5ce385ba1496d3c99
Hannes Moser <box@hannesmoser.at>
2024-07-23 15:50:42 +0200

    Update to latest Node version (#6231)

--- examples/vite-vanilla-ts/vite.config.ts
+++ examples/vite-vanilla-ts/vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
 
 export default defineConfig({
-  plugins: [
-    // required to support top level await
-    topLevelAwait()
-  ]
+    esbuild: {
+        supported: {
+            'top-level-await': true
+        },
+    },
 });
