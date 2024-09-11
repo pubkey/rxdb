1444721c6caff03f91dab47836411508e7eddc60
pubkey <8926560+pubkey@users.noreply.github.com>
2024-09-11 13:06:18 +0200

    ADD chat

--- docs-src/docusaurus.config.ts
+++ docs-src/docusaurus.config.ts
@@ -57,11 +57,11 @@ const config: Config = {
       type: 'text/javascript',
       async: true,
     },
-    // {
-    //   src: 'https://leadbooster-chat.pipedrive.com/assets/loader.js',
-    //   type: 'text/javascript',
-    //   async: true
-    // }
+    {
+      src: 'https://leadbooster-chat.pipedrive.com/assets/loader.js',
+      type: 'text/javascript',
+      async: true
+    }
     // {
     //   src: 'https://www.googletagmanager.com/gtag/js?id=G-62D63SY3S0',
     //   async: true,
