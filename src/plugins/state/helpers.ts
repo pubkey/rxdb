307d872c420bc2e4b4ed9df647e0e2ac4b1c1d57
pubkey <8926560+pubkey@users.noreply.github.com>
2024-10-06 13:24:53 +0200

    FIX RxState.property$ should emit stable reference #6462

--- src/plugins/state/helpers.ts
+++ src/plugins/state/helpers.ts
@@ -1,3 +1,5 @@
+import { overwritable } from '../../overwritable.ts';
+import { newRxError } from '../../rx-error.ts';
 import type { DeepReadonly, RxJsonSchema } from '../../types';
 import type { RxStateDocument } from './types';
 
@@ -65,4 +67,22 @@ export function nextRxStateId(lastId?: string): string {
     const next = parsed + 1;
     const nextString = next.toString();
     return nextString.padStart(RX_STATE_ID_LENGTH, '0');
+}
+
+
+
+/**
+ * Only non-primitives can be used as a key in WeakMap
+ */
+export function isValidWeakMapKey(key: any) {
+    // This method is slow and must only be used in dev-mode!
+    if (!overwritable.isDevMode()) {
+        throw newRxError('SNH');
+    }
+    try {
+        new WeakMap().set(key, {});
+        return true;
+    } catch (err) {
+        return false;
+    }
 }
