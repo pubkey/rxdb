ecffb9c5e3138d8e6740ca0cb35ec223beac4e0b
pubkey <8926560+pubkey@users.noreply.github.com>
2024-12-05 16:30:12 +0100

    ADD test for parallel attachments access

--- test/unit/attachments.test.ts
+++ test/unit/attachments.test.ts
@@ -1,6 +1,6 @@
 import assert from 'assert';
 import config, { describeParallel } from './config.ts';
-import AsyncTestUtil from 'async-test-util';
+import AsyncTestUtil, { randomBoolean } from 'async-test-util';
 
 import {
     schemaObjects,
@@ -10,7 +10,8 @@ import {
     isNode,
     isDeno,
     HumanDocumentType,
-    getEncryptedStorage
+    getEncryptedStorage,
+    randomStringWithSpecialChars
 } from '../../plugins/test-utils/index.mjs';
 import {
     clone,
@@ -29,7 +30,10 @@ import {
     createBlob,
     blobToString,
     RxDocumentWriteData,
-    addRxPlugin
+    addRxPlugin,
+    flatClone,
+    randomOfArray,
+    promiseWait
 } from '../../plugins/core/index.mjs';
 import { RxDBMigrationSchemaPlugin } from '../../plugins/migration-schema/index.mjs';
 addRxPlugin(RxDBMigrationSchemaPlugin);
@@ -302,6 +306,58 @@ describeParallel('attachments.test.ts', () => {
             assert.ok(hasThrown);
 
             c.database.remove();
+        });
+    });
+    describe('atomicity', () => {
+        it('write and read many attachments in parallel', async () => {
+            const c = await humansCollection.createAttachments(1);
+            const doc = await c.findOne().exec(true);
+
+            let x = 10;
+            const attachmentIds: string[] = [];
+            const promises: Promise<any>[] = [];
+            console.log('######################################');
+            console.log('######################################');
+            console.log('######################################');
+            while (x > 0) {
+                x--;
+                if (randomBoolean()) {
+                    // add
+                    const id = randomStringWithSpecialChars(10, 14) + '-att';
+                    promises.push(
+                        doc.putAttachment({
+                            id,
+                            data: createBlob(randomStringWithSpecialChars(10, 14), 'text/plain'),
+                            type: 'text/plain'
+                        }).then((attachment) => {
+                            attachment.getStringData();
+                            console.log('stored: ' + id);
+                            attachmentIds.push(id);
+                        })
+                    );
+                } else {
+                    // read
+                    promises.push(
+                        (async () => {
+                            while (true) {
+                                const id = randomOfArray(attachmentIds);
+                                if (!id) {
+                                    await promiseWait(10);
+                                } else {
+                                    console.log('start read: ' + id);
+                                    const attachment = doc.getLatest().getAttachment(id);
+                                    await ensureNotFalsy(attachment, 'missing attachment ' + id).getData();
+                                    console.log('got');
+                                    break;
+                                }
+                            }
+                        })()
+                    );
+                }
+            }
+
+            await Promise.all(promises);
+            await c.database.close();
         });
     });
     describe('RxAttachment.getData()', () => {
