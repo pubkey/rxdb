148a09346084f99c917fdad9ff5981b89fe9f118
pubkey <8926560+pubkey@users.noreply.github.com>
2024-12-25 12:27:13 +0100

    ADD more reviews

--- docs-src/src/components/review-block.tsx
+++ docs-src/src/components/review-block.tsx
@@ -1,78 +1,102 @@
 import Slider from 'react-slick';
 
 
-export const REVIEW_ITEMS = [
-    {
-        label: 'Readwise, USA',
-        description: 'We use RxDB because it works across multiple platforms and we need to store of a great deal of data, some users have tens of thousands of documents! RxDB was the only cross-platform, offline-first solution with good enough performance to meet our needs.',
-        href: 'https://readwise.io/',
-        target: '_blank',
-        logo: '/files/companies/readwise.svg',
-        logoStyle: 'slider-logo-black '
-    },
-    {
-        label: 'myAgro, Africa',
-        description: 'Transitioning to RxDB was a breath of fresh air due to its comprehensive features, including schema migration, real-time replication, conflict resolution, and reactive programming.',
-        href: 'https://www.myagro.org/',
-        target: '_blank',
-        logo: '/files/companies/myagro.svg',
-        logoStyle: 'slider-logo-black ',
-    },
-    {
-        label: 'MoreApp, Germany',
-        description: `We provide a mobile app that is used by people in the
+export const REVIEW_ITEMS: {
+    label: string;
+    description: string;
+    href: string;
+    target: '_blank';
+    logo: string;
+    logoStyle: 'slider-logo-black ' | 'slider-logo-white ';
+}[] = [
+        {
+            label: 'Readwise, USA',
+            description: 'We use RxDB because it works across multiple platforms and we need to store of a great deal of data, some users have tens of thousands of documents! RxDB was the only cross-platform, offline-first solution with good enough performance to meet our needs.',
+            href: 'https://readwise.io/',
+            target: '_blank',
+            logo: '/files/companies/readwise.svg',
+            logoStyle: 'slider-logo-black '
+        },
+        {
+            label: 'SafeEx, Denmark',
+            description: 'We rely on RxDB to power our offline-first inspection software. Its seamless offline capabilities keep data accurate even under poor connectivity, and switching between different database providers through one interface significantly simplifies our development. RxDB\'s flexibility positions us to confidently expand into more platforms and environments.',
+            href: 'https://www.safeex.com/',
+            target: '_blank',
+            logo: '/files/companies/safeex.svg',
+            logoStyle: 'slider-logo-white '
+        },
+        {
+            label: 'WebWare, Italy',
+            description: 'We built an offline-first application for technicians worldwide using RxDB. Its well-thought-out logic, extensive features, and the ability to maintain full control make RxDB a perfect fit for our needs. It ensures reliable performance, even in challenging network conditions, resulting in a seamless global maintenance solution.',
+            href: 'https://www.webware.dev/',
+            target: '_blank',
+            logo: '/files/companies/webware.svg',
+            logoStyle: 'slider-logo-black '
+        },
+        {
+            label: 'myAgro, Africa',
+            description: 'We rely on RxDB to manage all our data in one place. Our custom store became unwieldy, so we switched to RxDB for schema migrations, real-time replication, conflict resolution, and reactive programming. Its push and pull handlers also integrate smoothly with our existing APIs.',
+            
+            href: 'https://www.myagro.org/',
+            target: '_blank',
+            logo: '/files/companies/myagro.svg',
+            logoStyle: 'slider-logo-black ',
+        },
+        {
+            label: 'MoreApp, Germany',
+            description: `We provide a mobile app that is used by people in the
         field to fill in valuable information like inspections,
         surveys and audits. Our users don't always have access to the internet, building from an Offline-first approach with RxDB allows us to have the data integrity we need without being online.`,
-        href: 'https://moreapp.com/',
-        target: '_blank',
-        logo: '/files/companies/moreapp.png',
-        logoStyle: 'slider-logo-black ',
-    },
-    {
-        label: 'ALTGRAS, Guinea',
-        description: `We use RxDB to create applications capable of being
+            href: 'https://moreapp.com/',
+            target: '_blank',
+            logo: '/files/companies/moreapp.png',
+            logoStyle: 'slider-logo-black ',
+        },
+        {
+            label: 'ALTGRAS, Guinea',
+            description: `We use RxDB to create applications capable of being
         used in the most remote areas where Internet access is
         really a challenge.`,
-        href: 'https://altgras.com/',
-        target: '_blank',
-        logo: '/files/companies/altgras.png',
-        logoStyle: 'slider-logo-white ',
-    },
-    {
-        label: 'WooCommerce POS, Australia',
-        description: `We use RxDB to provide an offline first, cross platform
+            href: 'https://altgras.com/',
+            target: '_blank',
+            logo: '/files/companies/altgras.png',
+            logoStyle: 'slider-logo-white ',
+        },
+        {
+            label: 'WooCommerce POS, Australia',
+            description: `We use RxDB to provide an offline first, cross platform
         point of sale system. With RxDB we could create a web-, desktop- and mobile app using the same code base.`,
-        href: 'https://wcpos.com/',
-        target: '_blank',
-        logo: '/files/companies/woopos.png',
-        logoStyle: 'slider-logo-black ',
-    },
-    {
-        label: 'atroo GmbH, Germany',
-        description: `RxDB is a main component in building offline-ready
+            href: 'https://wcpos.com/',
+            target: '_blank',
+            logo: '/files/companies/woopos.png',
+            logoStyle: 'slider-logo-black ',
+        },
+        {
+            label: 'atroo GmbH, Germany',
+            description: `RxDB is a main component in building offline-ready
         multichannel apps. It has become our default stack for
         this kind of apps.`,
-        href: 'https://atroo.de/',
-        target: '_blank',
-        logo: '/files/companies/atroo.png',
-        logoStyle: 'slider-logo-black ',
-    },
-    {
-        label: 'Nutrien, Canada',
-        description: 'With RxDB we have built an offline capable Progressive Web Application that is used by our borer operators to report on conditions at the mineface.',
-        href: 'https://www.nutrien.com/',
-        target: '_blank',
-        logo: '/files/companies/nutrien.svg',
-        logoStyle: 'slider-logo-black ',
-    },
-    // {
-    //     label: "Mapgl Grafana plugins",
-    //     description: `"We use RxDB to store clients network topology modifications made with our map plugin visual editor"`,
-    //     href: "https://raw.githubusercontent.com/vaduga/mapgl-community/",
-    //     logo: "https://raw.githubusercontent.com/vaduga/mapgl-community/main/src/img/logo.png",
-    //     logoStyle: 'slider-logo-black ',
-    // },
-];
+            href: 'https://atroo.de/',
+            target: '_blank',
+            logo: '/files/companies/atroo.png',
+            logoStyle: 'slider-logo-black ',
+        },
+        {
+            label: 'Nutrien, Canada',
+            description: 'With RxDB we have built an offline capable Progressive Web Application that is used by our borer operators to report on conditions at the mineface.',
+            href: 'https://www.nutrien.com/',
+            target: '_blank',
+            logo: '/files/companies/nutrien.svg',
+            logoStyle: 'slider-logo-white ',
+        },
+        // {
+        //     label: "Mapgl Grafana plugins",
+        //     description: `"We use RxDB to store clients network topology modifications made with our map plugin visual editor"`,
+        //     href: "https://raw.githubusercontent.com/vaduga/mapgl-community/",
+        //     logo: "https://raw.githubusercontent.com/vaduga/mapgl-community/main/src/img/logo.png",
+        //     logoStyle: 'slider-logo-black ',
+        // },
+    ];
 
 
 export function ReviewsBlock() {
@@ -154,7 +178,7 @@ export function ReviewsBlock() {
                             className="review-img"
                             alt="RxDB review quote"
                             loading="lazy"
-                            />
+                        />
                         <h3>{item.description}</h3>
                         <div className="slider-profile">
                             <img
@@ -167,6 +191,7 @@ export function ReviewsBlock() {
                                 <p className="developer">{item.label}</p>
                                 <a
                                     href={item.href}
+                                    rel='nofollow noopener'
                                     target={item.target ? '_blank' : '_self'}
                                     className="company-link"
                                 >
