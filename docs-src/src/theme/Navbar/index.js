88a77b98d0f0875f06ad0cce5268577409539e19
Tim Sekiguchi <sekiguchi.tim@gmail.com>
2025-01-04 20:21:12 -0800

    fix: add back scrollbar, make sure width matches the navbar on homepage and docs.

--- docs-src/src/theme/Navbar/index.js
+++ docs-src/src/theme/Navbar/index.js
@@ -1,47 +1,46 @@
 import React from 'react';
-// import {
-//   useState,
-//   useEffect
-// } from 'react';
+import {
+  useState,
+  useEffect
+} from 'react';
 import Navbar from '@theme-original/Navbar';
 
 export default function NavbarWrapper(props) {
+  const [isHomepage, setIsHomepage] = useState(false);
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const handleScroll = () => {
+    const position = currentScrollPercentage().toFixed(2);
+    setScrollPosition(position);
+  };
 
+  useEffect(() => {
+    setIsHomepage(window.location.pathname === '/' || window.location.pathname === '/premium' || window.location.pathname === '/consulting');
 
-//   const styles = {
-//     scrollIndicator: {
-//       position: 'fixed',
-//       display: 'block',
-//       zIndex: 10,
-//       height: 3,
-//       backgroundColor: 'var(--color-top)',
-//       top: 60,
-//       borderTopRightRadius: 2,
-//       borderBottomRightRadius: 2,
-//     }
-//   };
+    window.addEventListener('scroll', handleScroll, { passive: true });
 
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
 
-
-//   const [scrollPosition, setScrollPosition] = useState(0);
-//   const handleScroll = () => {
-//     const position = currentScrollPercentage().toFixed(2);
-//     setScrollPosition(position);
-//   };
-
-//   useEffect(() => {
-//     window.addEventListener('scroll', handleScroll, { passive: true });
-
-//     return () => {
-//       window.removeEventListener('scroll', handleScroll);
-//     };
-//   }, []);
-
-
+  const styles = {
+    scrollIndicator: {
+      position: 'fixed',
+      display: 'block',
+      zIndex: 10,
+      height: 1.5,
+      backgroundColor: 'var(--color-top)',
+      top: 62.5,
+      borderTopRightRadius: 2,
+      borderBottomRightRadius: 2,
+      maxWidth: isHomepage ? 'none' : '88rem',
+      left: isHomepage ? 0 : 'calc((100% - 88rem) / 2)',
+    }
+  };
   return (
     <>
       <Navbar {...props} />
-      {/* <div style={{ ...styles.scrollIndicator, ...{ width: scrollPosition + '%' } }}></div> */}
+      {<div style={{ ...styles.scrollIndicator, ...{ width: scrollPosition + '%' } }}></div>}
     </>
   );
 }
@@ -49,6 +48,6 @@ export default function NavbarWrapper(props) {
 /**
  * @link https://stackoverflow.com/a/2387222
  */
-// function currentScrollPercentage() {
-//   return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
-// }
+function currentScrollPercentage() {
+  return ((document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100);
+}
