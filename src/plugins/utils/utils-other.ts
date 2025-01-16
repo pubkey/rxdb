dc95b497ff8c06bbe1e8091ddd4fcd6bfacd1809
pubkey <8926560+pubkey@users.noreply.github.com>
2025-01-16 20:45:49 +0100

    ADD fetch with fixed headers to utils

--- src/plugins/utils/utils-other.ts
+++ src/plugins/utils/utils-other.ts
@@ -41,3 +41,18 @@ export function nameFunction<T>(name: string, body: T): T {
     // @ts-ignore
     return { [name](...args) { return body.apply(this, args) } }[name];
 }
+
+
+export function customFetchWithFixedHeaders(headers: any){
+    function customFetch(url: string | URL, options: any = {}) {
+        // Ensure options object exists and headers property is initialized
+        options.headers = {
+            ...headers,              // include default custom headers
+            ...(options.headers || {})            // merge any headers passed in the function call
+        };
+
+        // Call the original fetch with the modified options
+        return fetch(url, options);
+    }
+    return customFetch;
+}
