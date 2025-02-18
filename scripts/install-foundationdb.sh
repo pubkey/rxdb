0e339912a097fc8796d96eff35bf863e9f081d34
nickchomey <88559987+nickchomey@users.noreply.github.com>
2025-02-17 22:20:22 -0600

    Update install-foundationdb.sh versions

--- scripts/install-foundationdb.sh
+++ scripts/install-foundationdb.sh
@@ -2,13 +2,13 @@
 set -e
 
 echo "# installing FoundationDB client:"
-wget -O foundationdb-client.deb https://github.com/apple/foundationdb/releases/download/6.3.23/foundationdb-clients_6.3.23-1_amd64.deb
+wget -O foundationdb-client.deb https://github.com/apple/foundationdb/releases/download/7.3.59/foundationdb-clients_7.3.59-1_amd64.deb
 sudo dpkg -i foundationdb-client.deb
 rm foundationdb-client.deb
 
 
 echo "# installing FoundationDB server:"
-wget -O foundationdb-server.deb https://github.com/apple/foundationdb/releases/download/6.3.23/foundationdb-server_6.3.23-1_amd64.deb
+wget -O foundationdb-server.deb https://github.com/apple/foundationdb/releases/download/7.3.59/foundationdb-server_7.3.59-1_amd64.deb
 sudo dpkg -i foundationdb-server.deb
 rm foundationdb-server.deb
 
