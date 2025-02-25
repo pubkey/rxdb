5d30822b48ee6862f045120cbc3b5169aead41b0
pubkey <8926560+pubkey@users.noreply.github.com>
2025-02-25 19:11:58 +0100

    FIX ci

--- examples/react-native/storage.js
+++ examples/react-native/storage.js
@@ -5,7 +5,7 @@
  */
 import { wrappedValidateAjvStorage } from 'rxdb/plugins/validate-ajv';
 import { getRxStorageSQLiteTrial, getSQLiteBasicsQuickSQLite } from 'rxdb/plugins/storage-sqlite';
-import { open } from 'react-native-quick-sqlite';
+import { open } from 'react-native-nitro-sqlite';
 export const STORAGE = wrappedValidateAjvStorage({
     storage: getRxStorageSQLiteTrial({
         sqliteBasics: getSQLiteBasicsQuickSQLite(open)
