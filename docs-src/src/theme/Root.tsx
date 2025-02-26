d2aff439ba7d72463e781dbfadd05348fb776339
pubkey <8926560+pubkey@users.noreply.github.com>
2025-02-26 14:44:29 +0100

    ENABLE history hack

--- docs-src/src/theme/Root.tsx
+++ docs-src/src/theme/Root.tsx
@@ -474,79 +474,80 @@ function startAnalytics() {
 
 
 
-    // function parseQueryParams(url) {
-    //     const urlSearchParams = new URL(url).searchParams;
-    //     const queryParams = Object.fromEntries(urlSearchParams.entries());
-    //     return queryParams;
-    // }
+
 
     /**
      * History hack,
      * show landingpage on back from somewhere else.
      */
-    // function historyHack() {
-    //     console.log('document.referrer: ' + document.referrer);
-    //     console.log(' window.location.hostname: ' + window.location.hostname);
-    //     const reloadWait = 100;
-    //     const queryParamFlag = 'history-back';
-    //     const originalUrl = location.href;
-    //     const queryParams = parseQueryParams(window.location);
-    //     let prePopstateUrl = location.href;
-    //     window.addEventListener('popstate', function (_event) {
-    //         const from = prePopstateUrl;
-    //         const to = location.href;
-    //         // console.log('from : ' + from);
-    //         // console.log('to : ' + to);
-    //         prePopstateUrl = location.href;
+    function parseQueryParams(url) {
+        const urlSearchParams = new URL(url).searchParams;
+        const queryParams = Object.fromEntries(urlSearchParams.entries());
+        return queryParams;
+    }
+    function historyHack() {
+        console.log('document.referrer: ' + document.referrer);
+        console.log(' window.location.hostname: ' + window.location.hostname);
+        const reloadWait = 100;
+        const queryParamFlag = 'history-back';
+        const originalUrl = location.href;
+        const queryParams = parseQueryParams(window.location);
+        let prePopstateUrl = location.href;
+        window.addEventListener('popstate', function (_event) {
+            const from = prePopstateUrl;
+            const to = location.href;
+            // console.log('from : ' + from);
+            // console.log('to : ' + to);
+            prePopstateUrl = location.href;
 
-    //         if (
-    //             parseQueryParams(from)[queryParamFlag] &&
-    //             document.referrer &&
-    //             parseQueryParams(document.referrer)[queryParamFlag]
-    //         ) {
-    //             return;
-    //         }
+            if (
+                parseQueryParams(from)[queryParamFlag] &&
+                document.referrer &&
+                parseQueryParams(document.referrer)[queryParamFlag]
+            ) {
+                return;
+            }
 
-    //         if (
-    //             new URL(from).pathname === '/' &&
-    //             parseQueryParams(to)[queryParamFlag]
-    //         ) {
-    //             history.forward();
-    //             setTimeout(() => {
-    //                 location.reload();
-    //             }, reloadWait);
-    //             return;
-    //         }
+            if (
+                new URL(from).pathname === '/' &&
+                parseQueryParams(to)[queryParamFlag]
+            ) {
+                history.forward();
+                setTimeout(() => {
+                    location.reload();
+                }, reloadWait);
+                return;
+            }
 
-    //         if (
-    //             new URL(from).pathname === new URL(to).pathname
-    //         ) {
-    //             return;
-    //         }
+            if (
+                new URL(from).pathname === new URL(to).pathname
+            ) {
+                return;
+            }
 
-    //         setTimeout(() => {
-    //             location.reload();
-    //         }, reloadWait);
-    //     }, {
-    //         passive: true
-    //     });
+            setTimeout(() => {
+                location.reload();
+            }, reloadWait);
+        }, {
+            passive: true
+        });
 
 
-    //     if (queryParams[queryParamFlag]) {
-    //         history.back();
-    //         setTimeout(function () {
-    //             history.replaceState(null, document.title, '/');
-    //         }, 200);
-    //     } else if (
-    //         document.referrer &&
-    //         new URL(document.referrer).hostname !== window.location.hostname &&
-    //         location.pathname !== '/'
-    //     ) {
-    //         history.pushState(null, document.title, '/?' + queryParamFlag + '=true');
-    //         history.pushState(null, document.title, originalUrl);
-    //     }
-    // }
-    // historyHack();
+        if (queryParams[queryParamFlag]) {
+            history.back();
+            setTimeout(function () {
+                history.replaceState(null, document.title, '/');
+            }, 200);
+        } else if (
+            document.referrer &&
+            new URL(document.referrer).hostname !== window.location.hostname &&
+            location.pathname !== '/'
+        ) {
+            history.pushState(null, document.title, '/?' + queryParamFlag + '=true');
+            history.pushState(null, document.title, originalUrl);
+        }
+    }
+    historyHack();
 
 
 
