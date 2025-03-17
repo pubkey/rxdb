62170362882b394e030911300131d78d3a0ebbf1
pubkey <8926560+pubkey@users.noreply.github.com>
2025-03-17 17:54:38 +0100

    FIX build

--- docs-src/src/components/a-b-tests.tsx
+++ docs-src/src/components/a-b-tests.tsx
@@ -1,9 +1,9 @@
 import { randomOfArray } from '../../../plugins/utils';
 import { ScrollToSection, SemPage } from '../pages';
-import { HeroSection_A } from './hero-section/T4_hero_a';
 import { HeroSection_B } from './hero-section/T4_hero_b';
-import { HeroSection_C } from './hero-section/T4_hero_c';
-import { HeroSection_D } from './hero-section/T4_hero_d';
+// import { HeroSection_A } from './hero-section/T4_hero_a';
+// import { HeroSection_C } from './hero-section/T4_hero_c';
+// import { HeroSection_D } from './hero-section/T4_hero_d';
 
 const CURRENT_TEST_RUN = {
     id: 'TX', // test hero page content type
