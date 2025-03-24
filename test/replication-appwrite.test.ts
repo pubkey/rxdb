c36ecd6439ba6b4365f63717aeaced6414d9413b
pubkey <8926560+pubkey@users.noreply.github.com>
2025-03-24 13:19:21 +0100

    FIX random test failures

--- test/replication-appwrite.test.ts
+++ test/replication-appwrite.test.ts
@@ -268,7 +268,7 @@ describe('replication-appwrite.test.ts', function () {
             ensureReplicationHasNoErrors(replicationStateB);
             await replicationStateB.awaitInitialReplication();
 
-            await wait(100);
+            await wait(300);
             await replicationStateA.awaitInSync();
 
             await ensureCollectionsHaveEqualState(collectionA, collectionB, 'init sync');
@@ -278,14 +278,14 @@ describe('replication-appwrite.test.ts', function () {
             await replicationStateA.awaitInSync();
 
             await replicationStateB.awaitInSync();
-            await wait(100);
+            await wait(300);
             await ensureCollectionsHaveEqualState(collectionA, collectionB, 'after insert');
 
             // delete one
             await collectionB.findOne().remove();
             await replicationStateB.awaitInSync();
             await replicationStateA.awaitInSync();
-            await wait(100);
+            await wait(300);
             await ensureCollectionsHaveEqualState(collectionA, collectionB, 'after deletion');
 
             // insert many
@@ -309,7 +309,7 @@ describe('replication-appwrite.test.ts', function () {
             await replicationStateB.awaitInSync();
             await replicationStateA.awaitInSync();
             await replicationStateB.awaitInSync();
-            await wait(100);
+            await wait(300);
             await ensureCollectionsHaveEqualState(collectionA, collectionB, 'after insert both at same time');
 
             collectionA.database.close();
