29f079c03051156e6e682b8d3ab96c67cc55465f
pubkey <8926560+pubkey@users.noreply.github.com>
2025-05-22 13:33:49 +0200

    ADD `putAttachmentBase64()` and `getDataBase64()` to the attachments API so that attachments can be stored and read in runtimes that do not support `Blob`

--- src/types/rx-attachment.d.ts
+++ src/types/rx-attachment.d.ts
@@ -16,6 +16,21 @@ export type RxAttachmentCreator = {
     data: Blob;
 };
 
+export type RxAttachmentCreatorBase64 = {
+    id: string;
+    /**
+     * Content type like 'plain/text'
+     */
+    type: string;
+    /**
+     * The data of the attachment as base64.
+     */
+    data: string;
+
+    // must be set manually
+    length: number;
+};
+
 export declare class RxAttachment<RxDocumentType, OrmMethods = {}, Reactivity = unknown> {
     readonly doc: RxDocument<RxDocumentType, OrmMethods, Reactivity>;
     readonly id: string;
