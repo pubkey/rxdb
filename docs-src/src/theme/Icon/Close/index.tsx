a4a37100fb498078576d9204e2e1123b043c9263
pubkey <8926560+pubkey@users.noreply.github.com>
2025-09-23 15:47:49 +0200

    FIX lint

--- docs-src/src/theme/Icon/Close/index.tsx
+++ docs-src/src/theme/Icon/Close/index.tsx
@@ -2,13 +2,6 @@ import React, { type ReactNode } from 'react';
 import type { Props } from '@theme/Icon/Close';
 import { IconClose as IconCloseRx } from '@site/src/components/icons/close';
 
-export default function IconClose({
-  // width = 21,
-  // height = 21,
-  // color = 'currentColor',
-  // strokeWidth = 1.2,
-  // className,
-  // ...restProps
-}: Props): ReactNode {
-  return <IconCloseRx />
+export default function IconClose(_props: Props): ReactNode {
+  return <IconCloseRx />;
 }
