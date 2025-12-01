9e5b396c0d1bf61faf73f70b2777e6861ce5f395
Daniel Meyer <8926560+pubkey@users.noreply.github.com>
2025-12-01 10:12:38 +0100

    FIX #7532 Array type is omitted in GraphQL query builder (#7572)
    * FIX #7532 Array type is omitted in GraphQL query builder
    
    * FIX typings

--- src/plugins/replication-graphql/query-builder-from-rx-schema.ts
+++ src/plugins/replication-graphql/query-builder-from-rx-schema.ts
@@ -28,13 +28,13 @@ export function pullQueryBuilderFromRxSchema(
 
     const outputFields = generateGQLOutputFields({ schema, ignoreOutputKeys: input.ignoreOutputKeys })
     // outputFields.push(input.deletedField);    
-    
+
     const checkpointInputName = ucCollectionName + 'Input' + prefixes.checkpoint;
     const builder: RxGraphQLReplicationPullQueryBuilder<any> = (checkpoint: any, limit: number) => {
         const query = 'query ' + operationName + '($checkpoint: ' + checkpointInputName + ', $limit: Int!) {\n' +
             SPACING + SPACING + queryName + '(checkpoint: $checkpoint, limit: $limit) {\n' +
-            SPACING + SPACING + SPACING + 'documents {\n' + 
-            outputFields  + '\n' +
+            SPACING + SPACING + SPACING + 'documents {\n' +
+            outputFields + '\n' +
             SPACING + SPACING + SPACING + '}\n' +
             SPACING + SPACING + SPACING + 'checkpoint {\n' +
             SPACING + SPACING + SPACING + SPACING + input.checkpointFields.join('\n' + SPACING + SPACING + SPACING + SPACING) + '\n' +
@@ -71,7 +71,7 @@ export function pullStreamBuilderFromRxSchema(
     const query = 'subscription on' + ucfirst(ensureNotFalsy(prefixes.stream)) + '($headers: ' + headersName + ') {\n' +
         SPACING + queryName + '(headers: $headers) {\n' +
         SPACING + SPACING + SPACING + 'documents {\n' +
-        outputFields  + '\n' +
+        outputFields + '\n' +
         SPACING + SPACING + SPACING + '}\n' +
         SPACING + SPACING + SPACING + 'checkpoint {\n' +
         SPACING + SPACING + SPACING + SPACING + input.checkpointFields.join('\n' + SPACING + SPACING + SPACING + SPACING) + '\n' +
@@ -104,12 +104,12 @@ export function pushQueryBuilderFromRxSchema(
 
     const variableName = collectionName + prefixes.pushRow;
     const returnFields = generateGQLOutputFields({ schema: input.schema, spaceCount: 2 })
-    
+
     const builder: RxGraphQLReplicationPushQueryBuilder = (pushRows) => {
         const query = '' +
             'mutation ' + operationName + '($' + variableName + ': [' + ucCollectionName + 'Input' + prefixes.pushRow + '!]) {\n' +
             SPACING + queryName + '(' + variableName + ': $' + variableName + ') {\n' +
-            returnFields  + '\n' +
+            returnFields + '\n' +
             SPACING + '}\n' +
             '}';
 
@@ -159,10 +159,10 @@ function generateGQLOutputFields(options: GenerateGQLOutputFieldsOptions) {
     const { schema, spaceCount = 4, depth = 0, ignoreOutputKeys = [] } = options;
 
     const outputFields: string[] = [];
-    const properties = schema.properties 
+    const properties = schema.properties
     const NESTED_SPACING = SPACING.repeat(depth);
     const LINE_SPACING = SPACING.repeat(spaceCount);
-  
+
     for (const key in properties) {
         //only skipping top level keys that are in ignoreOutputKeys list
         if (ignoreOutputKeys.includes(key)) {
@@ -170,17 +170,24 @@ function generateGQLOutputFields(options: GenerateGQLOutputFieldsOptions) {
         }
 
         const value = properties[key];
+        if (value.type === "array" && value.items) {
+            outputFields.push(
+                LINE_SPACING + NESTED_SPACING + key + " {",
+                generateGQLOutputFields({ schema: value.items as any, spaceCount, depth: depth + 1 }),
+                LINE_SPACING + NESTED_SPACING + "}"
+            );
+        }
         if (value.type === "object") {
-          outputFields.push(
-            LINE_SPACING + NESTED_SPACING + key + " {",
-            generateGQLOutputFields({ schema: value, spaceCount, depth: depth + 1 }),
-            LINE_SPACING + NESTED_SPACING + "}"
-          );
+            outputFields.push(
+                LINE_SPACING + NESTED_SPACING + key + " {",
+                generateGQLOutputFields({ schema: value, spaceCount, depth: depth + 1 }),
+                LINE_SPACING + NESTED_SPACING + "}"
+            );
         } else {
             outputFields.push(LINE_SPACING + NESTED_SPACING + key);
         }
     }
-    
+
     return outputFields.join('\n');
 }
 
