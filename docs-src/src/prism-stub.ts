d02b59ebade30eaa04f15e4965bbb6d43799d64f
Siraj Chokshi <19193347+SirajChokshi@users.noreply.github.com>
2025-12-09 01:55:36 -0800

    Server rendered CodeBlocks with Shiki (#7590)
    * init
    
    * ts -> json
    
    * add
    
    * remove custom theme, use css variables
    
    * cleanup

---
+++ docs-src/src/prism-stub.ts
@@ -0,0 +1,11 @@
+/**
+ * Stub module to omit `prism-react-renderer` from the bundle.
+ */
+export const Prism = {};
+export const themes = {
+    dracula: { plain: {}, styles: [] },
+    palenight: { plain: {}, styles: [] },
+};
+export const Highlight = () => null;
+export default { Prism, themes, Highlight };
+
