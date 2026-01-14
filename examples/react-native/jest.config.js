091bdbd1957856b79e4cc0a2cd7c96a4f6e7511d
pubkey <8926560+pubkey@users.noreply.github.com>
2026-01-14 14:23:12 +0100

    FIX react native transform for jest

--- examples/react-native/jest.config.js
+++ examples/react-native/jest.config.js
@@ -1,16 +1,19 @@
-module.exports = {
-    "projects": [
-       { "preset": "jest-expo/ios" },
-       { "preset": "jest-expo/android" }
+const transformIgnorePatterns = [
+    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg|mingo)"
+  ];
+  
+  module.exports = {
+    projects: [
+      {
+        preset: "jest-expo/ios",
+        transformIgnorePatterns,
+      },
+      {
+        preset: "jest-expo/android",
+        transformIgnorePatterns,
+      },
     ],
-    transform: {},
-    globals: {
-        'ts-jest': {
-            babelConfig: true,
-        },
-    },
-    testRegex: '(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$',
-    transformIgnorePatterns: [
-        "node_modules/(?!((jest-)?react-native|@react-native(-community)?)|expo(nent)?|@expo(nent)?/.*|@expo-google-fonts/.*|react-navigation|@react-navigation/.*|@unimodules/.*|unimodules|sentry-expo|native-base|react-native-svg)"
-    ],
-};
+  
+    testRegex: "(/__tests__/.*|\\.(test|spec))\\.(ts|tsx|js)$",
+  };
+  
